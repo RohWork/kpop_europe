fd169e319a4799c966e16526dcd872f7e09a94aa
ROH <ROH@JKROH>
2022-09-04 11:10:05 +0900

    시작
    요청자 : 노진국

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,2 +1,6 @@
-<footer>Copyright(C) 2022.Roh.All rights reserved</footer>
+<div class="container"> 
+    <footer class="py-3 my-4">
+        <p class="text-center text-muted">Copyright(C) 2022.Roh.All rights reserved</p>
+    </footer>
+</div>
 </html>
