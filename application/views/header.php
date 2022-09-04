1cdce11efa5010eac4ba77b2680a2f193958c84c
ROH <ROH@JKROH>
2022-09-04 10:49:50 +0900

    시작
    요청자 : 노진국

--- application/views/header.php
+++ application/views/header.php
@@ -7,8 +7,7 @@
     <body>
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
           <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Simple header</span>
+            <span class="fs-4">Kppop In Europe</span>
           </a>
 
           <ul class="nav nav-pills">
