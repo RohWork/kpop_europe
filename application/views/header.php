7b0d0071f2faef0383197a2ff9630d9c3b2d44ed
ROH <ROH@JKROH>
2022-09-04 11:00:23 +0900

    시작
    요청자 : 노진국

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom container-fluid">
           <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <svg class="bi me-4" width="50%" height="32"><use xlink:href="#bootstrap"/></svg>
             <span class="fs-4">Kppop In Europe</span>
