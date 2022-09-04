0aeccfd9e038aa1fd4d100d407e8e4536c820004
ROH <ROH@JKROH>
2022-09-04 11:20:47 +0900

    시작
    요청자 : 노진국

--- application/views/main.php
+++ application/views/main.php
@@ -1,1 +1,3 @@
-main
+<div>
+    
+</div>
---
+++ application/views/sidebar.php
@@ -0,0 +1,17 @@
+<div class="text-center text-muted" style="width:100px">
+    <a class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+        <span class="fs-5 fw-semibold">Kpop</span>
+    </a>
+    <ul class="list-unstyled ps-0">
+        <li class="mb-1">
+            <button class="btn btn-toggle align-items-center rounded collapsed">
+                Kpop Party
+            </button>
+            <div class="btn btn-toggle align-items-center rounded collapsed">
+                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a class="link-dark rounded">Schedule</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</div>
