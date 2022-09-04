e760ffa264486690b714455353f61d76e441c5e7
ROH <ROH@JKROH>
2022-09-04 12:23:01 +0900

    시작
    요청자 : 노진국

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -51,7 +51,7 @@
             </button>
             <div class="collapse" id="club-collapse">
                 <ul class="btn-toggle-nav2 list-unstyled fw-normal pb-1 small">
-                     <a href="#"><span class="btn-toggle-nav2 list-unstyled fw-normal pb-1 small link-dark">Schedule</span></a>
+                     <a href="#"><span class="btn-toggle-nav2 list-unstyled pb-1 small link-dark">Schedule</span></a>
                 </ul>
             </div>
         </li>
