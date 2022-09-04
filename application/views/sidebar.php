1ec4f667887f2ff3de4d27aee36b094caeb6cef1
ROH <ROH@JKROH>
2022-09-04 11:54:57 +0900

    시작
    요청자 : 노진국

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -1,3 +1,4 @@
+<main>
 <link href="/asset/css/sidebar.css" rel="stylesheet">
 <div class="flex-shrink-0 p-3 bg-white" style="width:200px">
     <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
