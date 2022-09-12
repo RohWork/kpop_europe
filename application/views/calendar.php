f781c40de66d2ed4232da5e152b51e125cc74fa9
ROH <ROH@JKROH>
2022-09-12 12:09:26 +0900

    시작
    요청자 : 노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -41,22 +41,7 @@ $last_week = date('w', mktime(0, 0, 0, $month, $max_day, $year));
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <style>
-    font.holy {font-family: tahoma;font-size: 20px;color: #FF6C21;}
-    font.blue {font-family: tahoma;font-size: 20px;color: #0000FF;}
-    font.black {font-family: tahoma;font-size: 20px;color: #000000;}
-  </style>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
+
 <div class="container">
 <table class="table table-bordered table-responsive">
   <tr align="center" >
@@ -133,9 +118,6 @@ $last_week = date('w', mktime(0, 0, 0, $month, $max_day, $year));
   <?php } ?>
 </table>
 </div>
-
-</body>
-</html>
 
 
 
