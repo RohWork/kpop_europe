dcdd0201220f9f4d0f746563c1f14a78ae482c65
ROH <ROH@JKROH>
2022-09-12 13:04:05 +0900

    시작
    요청자 : 노진국

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -11,7 +11,7 @@ class Schedule_model extends CI_Model {
     public function get_schedule($country, $year , $month){
        
         $sSql = "SELECT ki.name, DATE_FORMAT(ki.start_date,'%Y-%m-%d') AS start_date, DATE_FORMAT(ki.end_date,'%Y-%m-%d') AS end_date FROM kpop_info AS ki 
-                 WHERE  LIKE '$year-$month%' AND ki.country ='$country'";
+                 WHERE ki.start_date LIKE '$year-$month%' AND ki.country ='$country'";
         
         $this->db->query($sSql);
         $this->db->result_array();
