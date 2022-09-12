c3bf1dde9e82ddee2245d84b0e71670095283483
ROH <ROH@JKROH>
2022-09-12 13:10:59 +0900

    시작
    요청자 : 노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -67,6 +67,7 @@
                         echo '</font>';
                     }
                     if(!empty($calendar[$year."-".sprintf('%02d',$month)."-".sprintf('%02d',$day)])){
+                        echo "<br/>";
                         echo $calendar[$year."-".sprintf('%02d',$month)."-".sprintf('%02d',$day)]['name'];
                     }
                     // 14. 날짜 증가
