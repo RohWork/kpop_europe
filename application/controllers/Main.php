4f88152d560cedfb586897247931ecae224ff06f
ROH <ROH@JKROH>
2022-09-12 12:18:22 +0900

    시작
    요청자 : 노진국

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -17,9 +17,53 @@ class Main extends CI_Controller {
     
     function calendar(){
         
+
+        //---- 오늘 날짜
+        $thisyear = date('Y'); // 4자리 연도
+        $thismonth = date('n'); // 0을 포함하지 않는 월
+        $today = date('j'); // 0을 포함하지 않는 일
+
+        //------ $year, $month 값이 없으면 현재 날짜
+        $data['year'] = $year = isset($this->input->get('year')) ? $this->input->get('year') : $thisyear;
+        $data['month'] =$month =  isset($this->input->get('month')) ?$this->input->get('month') : $thismonth;
+        $data['day'] = $day = isset($this->input->get('month')) ? $this->input->get('month') : $today;
+        
+
+        $data['prev_month'] = $prev_month = $month - 1;
+        $data['next_month'] = $next_month =  $month + 1;
+        $data['prev_year'] = $prev_year = $year;
+        
+        if ($month == 1) {
+            $data['prev_month'] = 12;
+            $data['prev_year'] = $year - 1;
+        } else if ($month == 12) {
+            $data['next_month'] = 1;
+            $data['next_year'] = $year + 1;
+        }
+        $data['preyear'] = $year - 1;
+        $data['nextyear'] = $year + 1;
+
+        $data['predate'] = date("Y-m-d", mktime(0, 0, 0, $month - 1, 1, $year));
+        $data['nextdate'] = date("Y-m-d", mktime(0, 0, 0, $month + 1, 1, $year));
+
+        // 1. 총일수 구하기
+        $data['max_day'] = $max_day =  date('t', mktime(0, 0, 0, $month, 1, $year)); // 해당월의 마지막 날짜
+        //echo '총요일수'.$max_day.'<br />';
+
+        // 2. 시작요일 구하기
+        $data['start_week'] = $start_week =  date("w", mktime(0, 0, 0, $month, 1, $year)); // 일요일 0, 토요일 6
+
+        // 3. 총 몇 주인지 구하기
+        $data['total_week'] = ceil(($max_day + $start_week) / 7);
+
+        // 4. 마지막 요일 구하기
+        $data['last_week'] = date('w', mktime(0, 0, 0, $month, $max_day, $year));
+
+        
+        
         $this->load->view('header');
         $this->load->view('sidebar');
-        $this->load->view('calendar');
+        $this->load->view('calendar',$data);
         $this->load->view('footer');
     }
 
