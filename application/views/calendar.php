0463d67b20fc254dfadee6a0c8a85264462f854b
ROH <ROH@JKROH>
2022-09-18 14:03:23 +0900

    시작
    요청자 : 노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -70,7 +70,10 @@
                         $cal_data = $calendar[$year."-".sprintf('%02d',$month)."-".sprintf('%02d',$day)];
                         
                         echo "<br/>";
-                        echo "<font style='font-size:10px;cursor:pointer' onclick=go_detail('".$cal_data['idx']."_".$cal_data['name']."')>".$cal_data['name']."</font>";
+                        ?>
+                       <font style='font-size:10px;cursor:pointer' onclick=go_detail("<?=$cal_data['idx']."_".$cal_data['name']?>")><?=$cal_data['name']?></font>
+              <?php
+                        
                     }
                     // 14. 날짜 증가
                     $day++;
