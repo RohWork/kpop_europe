2316360435f140cd151970dffa4a849ddd90549f
ROH <ROH@JKROH>
2022-09-18 13:22:18 +0900

    시작
    요청자 : 노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -88,7 +88,7 @@
         <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">공지</h5>
+                <h5 class="modal-title">detail</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
