689338a2b29793cd7a286d2ea8df913dae41a38c
ROH <ROH@JKROH>
2022-09-18 14:19:48 +0900

    시작
    요청자 : 노진국

--- application/views/detail.php
+++ application/views/detail.php
@@ -90,13 +90,21 @@
                     <?php
                       foreach($detail_img as $img){
                     ?>
-                        <div class="carousel-item active">
+                        <div class="carousel-item">
                         <img src="<?=$img['src']?>" class="d-block w-100" alt="<?=$img['title']?>">
                       </div>
                     <?php
                     }
                     ?>
                     </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                      </button>
+                      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                      </button>
                   </div>
 
                 </div>
