7e5e20c3840946605542a2e8d42093ef72b81bd2
ROH <ROH@JKROH>
2022-09-18 14:41:32 +0900

    시작
    요청자 : 노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -131,4 +131,11 @@
         console.log("click close"); 
     });
 
+    $('#detail_modal').on('show.bs.modal', function () {
+           $(this).find('.modal-body').css({
+                  width:'auto', //probably not needed
+                  height:'auto', //probably not needed 
+                  'max-height':'100%'
+           });
+    });
 </script>
