9e6b53140205de64910d3f84bbc754bd3d62291e
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-09-22 08:50:13 +0900

    리딩매직 CP 이전작업

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Mem extends CI_Controller {
+class Member extends CI_Controller {
 
     function __construct() {
         parent ::__construct();
