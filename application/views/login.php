4f6e63fab27687794f84cf91af262f778c928cfa
ROH <ROH@JKROH>
2022-09-24 15:17:11 +0900

    시작
    요청자 : 노진국

--- application/views/login.php
+++ application/views/login.php
@@ -15,7 +15,7 @@
      // to decode the credential response.
      
      //const responsePayload = decodeJwtResponse(response.credential);
-     const responsePayload = atob(response.credential.split('.')[1]);
+     const responsePayload = parseJwt(response.credential);
 
      console.log("ID: " + responsePayload.sub);
      console.log('Full Name: ' + responsePayload.name);
@@ -24,5 +24,18 @@
      console.log("Image URL: " + responsePayload.picture);
      console.log("Email: " + responsePayload.email);
   }
+  
+  
+  function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+  
+  
   </script>
 </html>
