3d2af2ca8e1ba901799d81a84052da3de5c03514
ROH <ROH@JKROH>
2022-09-24 15:10:53 +0900

    시작
    요청자 : 노진국

--- application/views/login.php
+++ application/views/login.php
@@ -5,9 +5,22 @@
     <script src="https://accounts.google.com/gsi/client" async defer></script>
     <div id="g_id_onload"
          data-client_id="238139026847-mudgpdqus93r42v9cpa8bmcsb4igto5e.apps.googleusercontent.com"
-         data-ux_mode="redirect"
-         data-login_uri="https://www.kpopineu.com/member/login_process">
+         data-callback="handleCredentialResponse">
     </div>
     <div class="g_id_signin" data-type="standard"></div>
   </body>
+  <script>
+    function handleCredentialResponse(response) {
+     // decodeJwtResponse() is a custom function defined by you
+     // to decode the credential response.
+     const responsePayload = decodeJwtResponse(response.credential);
+
+     console.log("ID: " + responsePayload.sub);
+     console.log('Full Name: ' + responsePayload.name);
+     console.log('Given Name: ' + responsePayload.given_name);
+     console.log('Family Name: ' + responsePayload.family_name);
+     console.log("Image URL: " + responsePayload.picture);
+     console.log("Email: " + responsePayload.email);
+  }
+  </script>
 </html>
