e22233a6e3efe3175f1fa715b081a2ce66c84023
ROH <ROH@JKROH>
2022-10-03 10:15:57 +0900

    시작
    요청자 : 노진국

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -43,6 +43,8 @@ class Member extends CI_Controller {
         $join_history = $this->mem_md->get_member($email);
         $join_result = false;
         
+        var_dump($join_history);
+        
         if(empty($join_history)){
             
             $data = array(
