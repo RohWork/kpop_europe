b17d254d2b3c3754a5b4a2a56a9eaa743bb8bce5
ROH <ROH@JKROH>
2022-10-03 10:19:14 +0900

    시작
    요청자 : 노진국

--- application/views/login.php
+++ application/views/login.php
@@ -33,12 +33,12 @@
      //const responsePayload = decodeJwtResponse(response.credential);
      const responsePayload = parseJwt(response.credential);
 
-     console.log("ID: " + responsePayload.sub);
-     console.log('Full Name: ' + responsePayload.name);
-     console.log('Given Name: ' + responsePayload.given_name);
+     //console.log("ID: " + responsePayload.sub);
+     //console.log('Full Name: ' + responsePayload.name);
+     //console.log('Given Name: ' + responsePayload.given_name);
      console.log('Family Name: ' + responsePayload.family_name);
-     console.log("Image URL: " + responsePayload.picture);
-     console.log("Email: " + responsePayload.email);
+     //console.log("Image URL: " + responsePayload.picture);
+     //console.log("Email: " + responsePayload.email);
      
     var formData = new FormData();
 
