d72692e45b0221af7b78244e120861bc34414073
ROH <ROH@JKROH>
2022-10-03 09:59:16 +0900

    시작
    요청자 : 노진국

--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -2,7 +2,30 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
  
 class Member_model extends CI_Model {
+    
+    
+    function get_member($email){
+        
+        $sSql = "SELECT * FROM kpop_member WHERE email = $email ";
+        
+        $query = $this->db->query($sSql);
+        return $query->row_array();
+        
+        
+    }
+    
+    
+    function set_member($params){
+        
+        
 
+        $this->db->insert('mytable', $params);
+        
+        return $this->db->affected_rows();
+        
+    }
+    
+    
 }
 
 ?>
