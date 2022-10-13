34e090d87176079d01f459b800f315207f78d908
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-10-13 14:13:59 +0900

    리딩매직 로그폴더 추가

--- application/views/join.php
+++ application/views/join.php
@@ -1,8 +1,34 @@
-<?php
+<html>
+      <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 2 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Refobi</title>
+        <!-- 합쳐지고 최소화된 최신 CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+        <!-- 부가적인 테마 -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <!-- Custom styles for this template -->
+  </head>
+  <body>
+      <div class="container">
+          <div class="row">
+              <label class="col-3">Email</label>
+              <div class="col-9"><input type="text" id="user_email" name="user_email"></div>
+              
+          </div>
+          
+          
+          
+      </div>
+      
+      
+  </body>
+</html>
