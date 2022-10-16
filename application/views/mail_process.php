dd8843f90b3502851bae09013fdbd1bd086fa68e
ROH <ROH@JKROH>
2022-10-16 10:33:30 +0900

    시작
    요청자 : 노진국

--- application/views/mail_process.php
+++ application/views/mail_process.php
@@ -1,7 +1,11 @@
 <div class="container">
     <div class="row">
-        <div class="col">
-            <h1>Check to Your E-mail</h1>
+        <div class="col col-3">
+        </div>
+        <div class="col col-4">
+            <h1>Check to Your E-mail!</h1>
+        </div>
+        <div class="col col-3">
         </div>
     </div>
     
