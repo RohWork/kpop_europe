ee2dca5e0d9ea263499be703c033157a58d62df9
ROH <ROH@JKROH>
2022-10-16 22:20:14 +0900

    시작
    요청자 : 노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -1,23 +1,27 @@
-
+<style>
+    .a_border{
+        text-decoration: none;
+    }
+</style>
 
     <div class="container">
         <table class="table table-bordered table-responsive" style="width:700px">
           <tr align="center" >
             <td>
-                <a href=<?php echo '/main/calendar?year='.$preyear.'&month='.$month . '&day=1'; ?>>◀◀</a>
+                <a class="a_border" href=<?php echo '/main/calendar?year='.$preyear.'&month='.$month . '&day=1'; ?>>◀◀</a>
             </td>
             <td>
-                <a href=<?php echo '/main/calendar?year='.$prev_year.'&month='.$prev_month . '&day=1'; ?>>◀</a>
+                <a class="a_border" href=<?php echo '/main/calendar?year='.$prev_year.'&month='.$prev_month . '&day=1'; ?>>◀</a>
             </td>
             <td height="50" bgcolor="#FFFFFF" colspan="3">
-                <a href=<?php echo '/main/calendar?year=' . $thisyear . '&month=' . $thismonth . '&day=1'; ?>>
+                <a class="a_border" href=<?php echo '/main/calendar?year=' . $thisyear . '&month=' . $thismonth . '&day=1'; ?>>
                 <?php echo "&nbsp;&nbsp;" . $month . ' - ' . $year  . "&nbsp;&nbsp;"; ?></a>
             </td>
             <td>
-                <a href=<?php echo '/main/calendar?year='.$next_year.'&month='.$next_month.'&day=1'; ?>>▶</a>
+                <a class="a_border" href=<?php echo '/main/calendar?year='.$next_year.'&month='.$next_month.'&day=1'; ?>>▶</a>
             </td>
             <td>
-                <a href=<?php echo '/main/calendar?year='.$nextyear.'&month='.$month.'&day=1'; ?>>▶▶</a>
+                <a class="a_border" href=<?php echo '/main/calendar?year='.$nextyear.'&month='.$month.'&day=1'; ?>>▶▶</a>
             </td>
           </tr>
           <tr class="info">
