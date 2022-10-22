99bd88a96bb27f29d40e84f0f4e2d21c833621aa
ROH <ROH@JKROH>
2022-10-23 08:58:19 +0900

    시작
    요청자 : 노진국

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -1,6 +1,10 @@
 <main>
 <link href="/asset/css/sidebar.css" rel="stylesheet">
 <div class="flex-shrink-0 p-3 bg-white" style="width:200px">
+    
+    <div>
+        login
+    </div>
     <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
         <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
         <span class="fs-5 fw-semibold">Kpop</span>
