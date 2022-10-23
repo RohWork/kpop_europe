683e1152007338612cd4a7c864994f2bf642e07f
ROH <ROH@JKROH>
2022-10-23 09:04:31 +0900

    시작
    요청자 : 노진국

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -2,7 +2,7 @@
 <link href="/asset/css/sidebar.css" rel="stylesheet">
 <div class="flex-shrink-0 p-3 bg-white" style="width:200px">
     
-    <div class="mb-1 fs-5">
+    <div class="mb-1 fs-5 border-bottom">
         <?php if(empty($this->session->userdata('name') )){ ?>
             login
         <?php }else{ ?>
