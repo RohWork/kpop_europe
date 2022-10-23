4d86e09a435c1e4cea6ddf73a0310ed9d279af5c
ROH <ROH@JKROH>
2022-10-23 09:07:36 +0900

    시작
    요청자 : 노진국

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -4,7 +4,7 @@
     
     <div class="mb-1 border-bottom">
         <?php if(empty($this->session->userdata('name') )){ ?>
-        <button class="btn">login</button>
+        <button class="btn text-center">login</button>
         <?php }else{ ?>
             welecome! <br/>
             <?=$this->session->userdata('name')?>
