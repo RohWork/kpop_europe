495bca8fd5b73083a50f5f30af90080a68159cc4
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-10-28 13:19:56 +0900

    리딩매직 로그폴더 추가

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -169,7 +169,7 @@ class Member extends CI_Controller {
         $this->email->from("admin@kpopineu.com", "kpopineu");
         $this->email->to($mail);
         $this->email->subject('confirm to your address');
-        $this->email->message('<a href=https://www.kpopineu.com/member/confirm_email?id='.$result['id'].'>confirm to this set</a>');
+        $this->email->message('<a href=https://www.kpopineu.com/member/confirm_email?id='.$result['id'].'>confirm to this link</a>');
         
         if($this->email->send()) {
                  $this->load->view('mail_process');
