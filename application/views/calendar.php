95057b0f6aa47ba9e8d0b924090e4c585e7c6f56
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-11-24 09:29:32 +0900

    리딩매직 로그폴더 추가

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -199,7 +199,7 @@
                                     <input type="date" class="form-control" id="input_end_date" name="input_end_date"/>
                                 </div>
                             </div>
-                            <div class="row">
+                            <div class="row gap-3">
                                 <div class="col-2">
                                     <label class="form-label"><strong>Etc</strong></label>
                                 </div>
