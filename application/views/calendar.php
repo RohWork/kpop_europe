7f47f4ef6d5ba7869ebe15d469aee41f2547f5db
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-11-24 09:50:30 +0900

    리딩매직 로그폴더 추가

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -118,7 +118,7 @@
 
 <div id="insert_modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
         
-        <div class="modal-dialog modal-lg" style="max-height:85%;" role="document">
+        <div class="modal-dialog modal-lg"  role="document">
             <div class="modal-content" style="height:650px">
                 <div class="modal-header">
                     <h5 class="modal-title">insert</h5>
