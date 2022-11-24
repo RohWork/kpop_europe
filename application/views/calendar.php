88d9a85b94d472e6c9aa281ddcd5a91f9bff2810
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2022-11-24 09:18:10 +0900

    리딩매직 로그폴더 추가

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -214,7 +214,7 @@
                                     
                                 </div>
                                 
-                                <div class="col-9">
+                                <div class="col-10">
                                     <input type="text" id="input_image[]" class="form-control" name="input_image[]"/>
                                 </div>
                                 <div class="col-1">
