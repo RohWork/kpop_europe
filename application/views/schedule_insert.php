c09f3e42b877a62e1efa768deca14baf171c33e1
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-09 15:05:56 +0900

    국풍 수정

--- application/views/schedule_insert.php
+++ application/views/schedule_insert.php
@@ -4,11 +4,20 @@
             <div class="row">
                 <label class="form-label"><h3>calendar_insert</h3></label>
             </div>
-            <div class="row">
-                <label class="form-label bold col-2">Event</label>
-                <div class="col-4 col-offset-6"><input type="text" class="form-control" id="input_name" name="input_name"/><div>
-
+            <div class="row mt-1">
+                <label class="form-label col-2"><strong>Event</strong></label>
+                <div class="col-4 col-offset-6">
+                    <input type="text" class="form-control" id="input_name" name="input_name"/>
+                <div>
             </div>
+            <div class="row mt-1">
+
+                <label class="form-label col-2"><strong>Organizer</strong></label>
+
+                <div class="col-4 col-offset-6">
+                    <input type="text" class="form-control" id="input_company" name="input_company"/>
+                </div>
+            </div>     
         </form>
     </div>
 
