a86af14ff3d9ff4b119f69c3fb1ff8fec088f34d
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-09 14:53:19 +0900

    국풍 수정

--- application/views/insert_calendar.php
+++ application/views/insert_calendar.php
@@ -1,13 +1,15 @@
 <div>
     <div class="container">
-        <div class="row">
-            <label class="form-label">calendar_insert</div>
-        </div>
-        <div class="row">
-            <label class="form-label bold col-3">Event</label>
-            <div class="col-9"><input type="text" class="form-control" id="input_name" name="input_name"/><div>
-            
-        </div>
+        <form id="calendar_insert">
+            <div class="row">
+                <label class="form-label">calendar_insert</div>
+            </div>
+            <div class="row">
+                <label class="form-label bold col-3">Event</label>
+                <div class="col-9"><input type="text" class="form-control" id="input_name" name="input_name"/><div>
+
+            </div>
+        </form>
     </div>
 </div>
 </main>
