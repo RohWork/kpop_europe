2fc489342a363fcca22e27ed2ea63cb31a320b60
ROH <ROH@JKROH>
2022-12-11 17:15:59 +0900

    시작
    요청자 : 노진국

---
+++ application/models/Country_model.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Country_model extends CI_Model {
+ 
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    function insert_schedule($country){
+        
+        $params = array();
+        
+        $params['name'] = $country;
+        $params['writer'] = $this->session->userdata('name');
+        
+        $params['regi_date'] = date('Y-m-d h:i:s');
+        $this->db->insert('kpop_country', $country);
+        
+        return $this->db->insert_id();
+        
+    }
+}
