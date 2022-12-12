1ae069d3450c6b21f684265751427c9c9a33abc4
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-12 10:50:41 +0900

    국풍 수정

--- application/views/schedule_insert.php
+++ application/views/schedule_insert.php
@@ -5,15 +5,15 @@
                 <label class="form-label"><h3>Schedule Insert</h3></label>
             </div>
             <div class="row mt-1">
-                <label class="form-label col-2"><strong>Event</strong></label>
-                <div class="col-4 col-offset-6">
+                <label class="form-label col-md-2 col-xs-4"><strong>Event</strong></label>
+                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
                     <input type="text" class="form-control" id="input_name" name="input_name"/>
                 </div>
             </div>
             <div class="row mt-1">
-                <label class="form-label col-2"><strong>Country</strong></label>
+                <label class="form-label col-md-2 col-xs-4"><strong>Country</strong></label>
 
-                <div class="col-4 col-offset-6">
+                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
                         <select id="check_country" name="check_country" class="form-select">
                             <?php foreach($country as $cnt){ ?>
                                 <option value="<?=$cnt['idx']?>"><?=$cnt['name']?></option>
