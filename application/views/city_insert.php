48e16ec0b3a9ab187b606792add34d6cfcce6393
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-12 09:14:21 +0900

    국풍 수정

--- application/views/city_insert.php
+++ application/views/city_insert.php
@@ -28,7 +28,7 @@
             <div class="row mt-1" style="padding-top:50px">
                 
                 <div class="col-md-2 col-xs-2 "></div>
-                <div class="col-md-4 col-xs-4 col-xs-6 col-offset-6 col-xs-offset-6">
+                <div class="col-md-4 col-xs-4 col-xs-6 col-offset-6 col-xs-offset-6 text-center">
                     <button type="button" class="btn btn-success" id="btn_insert">INSERT</button>
                     <button type="button" class="btn btn-danger" id="btn_reset" onclick="form.reset();">RESET</button>
                 </div>
