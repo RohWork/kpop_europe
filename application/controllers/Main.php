be78f1caa3023b5b4f659b8eb33345148e161bb1
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-12 09:30:44 +0900

    국풍 수정

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -8,6 +8,7 @@ class Main extends CI_Controller {
         
         $this->load->model('Schedule_model', 'sch_md', TRUE);
         $this->load->model('Country_model', 'cont_md', TRUE);
+        $this->load->model('City_model', 'city_md', TRUE);
     }
     function index(){
         
@@ -137,7 +138,33 @@ class Main extends CI_Controller {
         $this->load->view('city_insert',$data);
         $this->load->view('footer');
     }
+    
+    function city_insert_ajax(){
         
+        $data = array();
+        
+        $data['result'] = 200;
+        $data['message'] = "";
+        
+        $params = array();
+        $params['country_idx'] = $this->input->post("check_country");
+        $params['name'] = $this->input->post("input_city");
+        
+        $result = $this->city_md->insert_city($params);
+        
+        if(!$result){
+            $data['result'] = 400;
+            $data['message'] = "data process error";
+        }
+        
+
+        
+        
+        header("Content-Type: application/json;");
+        echo json_encode($data);
+    }
+    
+    
     function organization_insert(){
         $data = array();
         
