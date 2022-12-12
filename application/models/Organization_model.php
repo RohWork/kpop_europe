8e0881244a56dba068c351524dfda58b110d3dce
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-12 09:51:57 +0900

    국풍 수정

--- application/models/Organization_model.php
+++ application/models/Organization_model.php
@@ -7,5 +7,26 @@ class Organization_model extends CI_Model {
     {
         parent::__construct();
     }
+ 
+    function insert_organization($params){
+        
+
+        $params['writer'] = $this->session->userdata('name');        
+        $params['regi_date'] = date('Y-m-d h:i:s');
+                
+        $this->db->insert('kpop_organization', $params);
+        
+        return $this->db->insert_id();
+        
+    }
     
+    function get_organization($country_idx, $city_idx){
+        
+
+        $sSql = "SELECT idx, country_idx, name FROM `kpop_city` where country_idx = $country_idx and city_idx = $city_idx";
+        
+        $query = $this->db->query($sSql);
+        return $query->result_array();
+        
+    }
 }
