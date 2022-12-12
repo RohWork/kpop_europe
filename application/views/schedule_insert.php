5023382cf337f5ece214df0f3ef8880cebea9d8b
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-12 11:10:39 +0900

    국풍 수정

--- application/views/schedule_insert.php
+++ application/views/schedule_insert.php
@@ -6,14 +6,14 @@
             </div>
             <div class="row mt-1">
                 <label class="form-label col-md-2 col-xs-4"><strong>Event</strong></label>
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <input type="text" class="form-control" id="input_name" name="input_name"/>
                 </div>
             </div>
             <div class="row mt-1">
                 <label class="form-label col-md-2 col-xs-4"><strong>Country</strong></label>
 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <select id="check_country" name="check_country" class="form-select">
                         <?php foreach($country as $cnt){ ?>
                             <option value="<?=$cnt['idx']?>"><?=$cnt['name']?></option>
@@ -25,7 +25,7 @@
             <div class="row mt-1">
                 <label class="form-label col-md-2 col-xs-4"><strong>City</strong></label>
 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <select class="form-select" id="input_country" name="input_country">
                         <option value=""></option>
                     </select>
@@ -35,7 +35,7 @@
 
                 <label class="form-label col-md-2 col-xs-4"><strong>Organizer</strong></label>
 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                         <select id="check_organization" name="check_organization" class="form-select">
                             <?php foreach($organization as $org){ ?>
                                 <option value="<?=$org['idx']?>"><?=$org['name']?></option>
@@ -46,7 +46,7 @@
             </div>
             <div class="row mt-1">
                 <label class="form-label col-md-2 col-xs-4"><strong>Hompage</strong></label>
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <input type="text" class="form-control" id="input_homepage" name="input_homepage"/>
                 </div>
             </div>
@@ -54,7 +54,7 @@
 
                 <label class="form-label col-md-2 col-xs-4"><strong>Address</strong></label>
 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <input type="text" class="form-control" id="input_addr" name="input_addr"/>
                 </div>
             </div>
@@ -62,7 +62,7 @@
 
                 <label class="form-label col-md-2 col-xs-4"><strong>Facebook</strong></label>
                 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <input type="text" class="form-control" id="input_face" name="input_face"/>
                 </div>
             </div>
@@ -70,7 +70,7 @@
 
                 <label class="form-label col-md-2 col-xs-4"><strong>Instagram</strong></label>
                 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <input type="text" class="form-control" id="input_insta" name="input_insta"/>
                 </div>
             </div>
@@ -78,7 +78,7 @@
  
                 <label class="form-label col-md-2 col-xs-4"><strong>Youtube</strong></label>
 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <input type="text" class="form-control" id="input_yout" name="input_yout"/>
                 </div>
             </div>
@@ -86,7 +86,7 @@
 
                 <label class="form-label col-md-2 col-xs-4"><strong>Date</strong></label>
 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
                     <div class="form_group">
                         <input type="date" class="form-control" id="input_start_date" name="input_start_date" value="<?=date('Y-m-d')?>"/>
                     </div>
@@ -102,7 +102,7 @@
                 
                 <label class="form-label col-md-2 col-xs-4"><strong>Etc</strong></label>
 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
 
                     <textarea id="input_remark" class="form-control" name="input_remark"> </textarea>
                 </div>
@@ -111,7 +111,7 @@
 
                 <label class="form-label col-md-2 col-xs-4"><strong>Image</strong></label>
                 
-                <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4" id="image_group" style="overflow-y: auto;height: 100px">
+                <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4" id="image_group" style="overflow-y: auto;height: 100px">
                     <div class="input-group mb-2 mt-1">
                         <input type="text" id="input_image[]" class="form-control i_img" name="input_image[]"/>
                         <button type="button" class="btn btn-primary" id="input_url">+</button>
