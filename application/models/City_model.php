f0b3b981418695495e8b0d31596c30fcec86d149
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-12 09:33:35 +0900

    국풍 수정

--- application/models/City_model.php
+++ application/models/City_model.php
@@ -8,4 +8,28 @@ class City_model extends CI_Model {
         parent::__construct();
     }
     
+        function insert_city($params){
+        
+
+        $params['writer'] = $this->session->userdata('name');        
+        $params['regi_date'] = date('Y-m-d h:i:s');
+        
+        $this->db->insert('kpop_city', $params);
+        
+        return $this->db->insert_id();
+        
+    }
+    
+    function get_city(){
+        
+        
+            
+        
+        $sSql = "SELECT idx, country_idx, name FROM `kpop_city`";
+        
+        $query = $this->db->query($sSql);
+        return $query->result_array();
+
+        
+    }
 }
