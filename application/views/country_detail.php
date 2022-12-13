7e2d56cf5513fe84ff30dee040671ace49fda09c
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-13 10:39:15 +0900

    국풍 수정

---
+++ application/views/country_detail.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="container" style="font-size: 12px">
+            <div class="row">
+                <div class="col-2">
+                    <label class="form-label bold"><strong>Country name</strong></label>
+                </div>
+                <div class="col-10">
+                    <p><?=$detail_info['name']?></p>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
