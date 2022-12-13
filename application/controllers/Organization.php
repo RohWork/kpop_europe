60837d16ed499699ead80879013ef72a44520bc0
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-13 14:13:23 +0900

    국풍 수정

--- application/controllers/Organization.php
+++ application/controllers/Organization.php
@@ -19,4 +19,33 @@ class Organization extends CI_Controller {
         $this->load->view('organization_list',$data);
         $this->load->view('footer');
     }
+    
+    function insert(){
+        $data = array();
+        
+        $this->load->view('header');
+        $this->load->view('sidebar');
+        $this->load->view('organization_insert',$data);
+        $this->load->view('footer');
+    }
+    
+    function insert_ajax(){
+        
+        $data = array();
+        
+        $data['result'] = 200;
+        $data['message'] = "";
+        
+        $organization = $this->input->post("input_organization");
+        $result = $this->org_md->insert_organization($organization);
+        
+        if(!$result){
+            $data['result'] = 400;
+            $data['message'] = "data process error";
+        }
+
+        
+        header("Content-Type: application/json;");
+        echo json_encode($data);
+    }
 }
