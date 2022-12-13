2cc63a43a8f6489182a56e1e5f8fb845b8dbde77
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-13 12:54:26 +0900

    국풍 수정

--- application/controllers/City.php
+++ application/controllers/City.php
@@ -16,4 +16,67 @@ class City extends CI_Controller {
         
         
     }
+    
+    function insert(){
+        
+        $data = array();
+        
+        $data['country'] = $this->cont_md->get_country();
+        
+        $this->load->view('header');
+        $this->load->view('sidebar');
+        $this->load->view('city_insert',$data);
+        $this->load->view('footer');
+    }
+    
+    function insert_ajax(){
+        
+        $data = array();
+        
+        $data['result'] = 200;
+        $data['message'] = "";
+        
+        $params = array();
+        $params['country_idx'] = $this->input->post("check_country");
+        $params['name'] = $this->input->post("input_city");
+       
+        
+        $result = $this->city_md->insert_city($params);
+        
+        if(!$result){
+            $data['result'] = 400;
+            $data['message'] = "data process error";
+        }
+        
+
+        
+        
+        header("Content-Type: application/json;");
+        echo json_encode($data);
+    }
+    
+    function get_ajax(){
+                
+        $data = array();
+        
+        $data['code'] = 200;
+        $data['message'] = "";
+        
+        
+        $country_idx = $this->input->post("country_idx");
+        
+        $result = $this->city_md->get_city($country_idx);
+        
+        if(!$result){
+            $data['code'] = 400;
+            $data['message'] = "data process error";
+        }else{
+            $data['result'] = $result;
+        }
+        
+        
+        header("Content-Type: application/json;");
+        echo json_encode($data);
+        
+    }
 }
