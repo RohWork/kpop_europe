0f42a90235486c2eaff25baa62cbbd1aad313bfb
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-13 13:28:04 +0900

    국풍 수정

--- application/models/City_model.php
+++ application/models/City_model.php
@@ -48,7 +48,7 @@ class City_model extends CI_Model {
                 . "where kc.state = 1 $where order by kc.country_idx asc, ko.idx desc";
         
         $query = $this->db->query($sSql);
-        return $query->result_array();
+        return $query->row_array();
     }
     
     function modify_city($city, $city_idx){
