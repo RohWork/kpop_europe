fe1e5e4554ac0f8599b2915515c87c595a95bb42
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-13 10:06:32 +0900

    국풍 수정

--- application/views/country_list.php
+++ application/views/country_list.php
@@ -1,6 +1,6 @@
     <div class="container">
         <div class="row">
-            <div class="col-md-2 col-xs2">
+            <div class="col-md-2 col-xs-2">
                 <button type="button" class="btn btn-primary" id="insert_button">INSERT</button>
             </div>
         </div>
