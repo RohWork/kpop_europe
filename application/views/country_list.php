800cd413b0cfde90a15ecefd86c43dd186843fea
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-13 10:50:14 +0900

    국풍 수정

--- application/views/country_list.php
+++ application/views/country_list.php
@@ -40,7 +40,7 @@
     <div id="detail_modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
         
         <div class="modal-dialog modal-sm" style="max-height:85%;" role="document">
-            <div class="modal-content" style="height:800px">
+            <div class="modal-content" style="height:auto">
                 <div class="modal-header">
                     <h5 class="modal-title">detail</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
