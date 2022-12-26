43fd1ad04ccdc9b665836b8afc3ca8d9d2b4dd95
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-26 16:14:49 +0900

    카카오용 성적페이지 추가

--- application/views/excel_result.php
+++ application/views/excel_result.php
@@ -1,56 +1,51 @@
-<div class="container" style="overflow: auto;">
-        <div class="row">
-            <div class="col-12">
-                <table class='table table-striped table-bordered' style='font-size:12px'>
-                    <tr>
-                        <th>result</th>
-                        <th>type</th>
-                        <th>party_name</th>
-                        <th>orgnizer</th>
-                        <th>date</th>
-                        <th>DOW</th>
-                        <th>country</th>
-                        <th>city</th>
-                        <th>address</th>
-                        <th>open</th>
-                        <th>close</th>
-                        <th>homepage</th>
-                        <th>instagram</th>
-                        <th>facebook</th>
-                    </tr>
 
-                    <?php
-                        foreach($excel as $ex){
-                    ?>        
-                    <tr>
-                        <td><?=$ex['result']?></td>
-                        <td><?=$ex['type']?></td>
-                        <td><?=$ex['party_name']?></td>
-                        <td><?=$ex['orgnizer']?></td>
-                        <td><?=$ex['date']?></td>
-                        <td><?=$ex['dow']?></td>
-                        <td><?=$ex['country']?></td>
-                        <td><?=$ex['city']?></td>
-                        <td><?=$ex['address']?></td>
-                        <td><?=$ex['open']?></td>
-                        <td><?=$ex['close']?></td>
-                        <td><?=$ex['homepage']?></td>
-                        <td><?=$ex['insta']?></td>
-                        <td><?=$ex['facebook']?></td>
-                    </tr>
+    <table class='table table-striped table-bordered' style='font-size:12px'>
+        <tr>
+            <th>result</th>
+            <th>type</th>
+            <th>party_name</th>
+            <th>orgnizer</th>
+            <th>date</th>
+            <th>DOW</th>
+            <th>country</th>
+            <th>city</th>
+            <th>address</th>
+            <th>open</th>
+            <th>close</th>
+            <th>homepage</th>
+            <th>instagram</th>
+            <th>facebook</th>
+        </tr>
 
-                    <?php        
-                        }
-                    ?>
+        <?php
+            foreach($excel as $ex){
+        ?>        
+        <tr>
+            <td><?=$ex['result']?></td>
+            <td><?=$ex['type']?></td>
+            <td><?=$ex['party_name']?></td>
+            <td><?=$ex['orgnizer']?></td>
+            <td><?=$ex['date']?></td>
+            <td><?=$ex['dow']?></td>
+            <td><?=$ex['country']?></td>
+            <td><?=$ex['city']?></td>
+            <td><?=$ex['address']?></td>
+            <td><?=$ex['open']?></td>
+            <td><?=$ex['close']?></td>
+            <td><?=$ex['homepage']?></td>
+            <td><?=$ex['insta']?></td>
+            <td><?=$ex['facebook']?></td>
+        </tr>
 
-                </table>
-                <center>
-                    <input type="button" value="HOME" onclick="go_home()"/>
-                </center>
-            </div>
-        </div>
+        <?php        
+            }
+        ?>
 
-    </div>
+    </table>
+    <center>
+        <input type="button" value="HOME" onclick="go_home()"/>
+    </center>
+
     <script>
         function go_home(){
             location.href="/main";
