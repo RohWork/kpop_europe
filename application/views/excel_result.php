44302701028eb8c148b1d9bde9c9fc6f28a6a569
ict_team <ict_team@DESKTOP-M7D75CU>
2022-12-26 16:01:50 +0900

    카카오용 성적페이지 추가

--- application/views/excel_result.php
+++ application/views/excel_result.php
@@ -1,52 +1,59 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <table class='table table-striped table-bordered' style='font-size:12px'>
-                <tr>
-                    <th>result</th>
-                    <th>type</th>
-                    <th>party_name</th>
-                    <th>orgnizer</th>
-                    <th>date</th>
-                    <th>DOW</th>
-                    <th>country</th>
-                    <th>city</th>
-                    <th>address</th>
-                    <th>start_date</th>
-                    <th>end_date</th>
-                    <th>homepage</th>
-                    <th>instagram</th>
-                    <th>facebook</th>
-                </tr>
-		
-                <?php
-                    foreach($excel as $ex){
-                ?>        
-                <tr>
-                    <td><?=$ex['result']?></td>
-                    <td><?=$ex['type']?></td>
-                    <td><?=$ex['party_name']?></td>
-                    <td><?=$ex['orgnizer']?></td>
-                    <td><?=$ex['date']?></td>
-                    <td><?=$ex['DOW']?></td>
-                    <td><?=$ex['country']?></td>
-                    <td><?=$ex['city']?></td>
-                    <td><?=$ex['address']?></td>
-                    <td><?=$ex['start_date']?></td>
-                    <td><?=$ex['end_date']?></td>
-                    <td><?=$ex['homepage']?></td>
-                    <td><?=$ex['instagram']?></td>
-                    <td><?=$ex['facebook']?></td>
-                </tr>
-                        
-                <?php        
-                    }
-                ?>
-                
-            </table>
-            
-            
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class='table table-striped table-bordered' style='font-size:12px'>
+                    <tr>
+                        <th>result</th>
+                        <th>type</th>
+                        <th>party_name</th>
+                        <th>orgnizer</th>
+                        <th>date</th>
+                        <th>DOW</th>
+                        <th>country</th>
+                        <th>city</th>
+                        <th>address</th>
+                        <th>start_date</th>
+                        <th>end_date</th>
+                        <th>homepage</th>
+                        <th>instagram</th>
+                        <th>facebook</th>
+                    </tr>
+
+                    <?php
+                        foreach($excel as $ex){
+                    ?>        
+                    <tr>
+                        <td><?=$ex['result']?></td>
+                        <td><?=$ex['type']?></td>
+                        <td><?=$ex['party_name']?></td>
+                        <td><?=$ex['orgnizer']?></td>
+                        <td><?=$ex['date']?></td>
+                        <td><?=$ex['DOW']?></td>
+                        <td><?=$ex['country']?></td>
+                        <td><?=$ex['city']?></td>
+                        <td><?=$ex['address']?></td>
+                        <td><?=$ex['start_date']?></td>
+                        <td><?=$ex['end_date']?></td>
+                        <td><?=$ex['homepage']?></td>
+                        <td><?=$ex['instagram']?></td>
+                        <td><?=$ex['facebook']?></td>
+                    </tr>
+
+                    <?php        
+                        }
+                    ?>
+
+                </table>
+                <center>
+                    <input type="button" value="HOME" onclick="go_home()"/>
+                </center>
+            </div>
         </div>
+
     </div>
-</div>
+    <script>
+        function go_home(){
+            location.href="/main";
+        }
+    </script>
 </body>
