24a07c4643c43b547df982d505a5fea3953bc15d
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-04 10:26:59 +0900

    2023.01.04 이용약관에 맞춰서 7일에서 14일로 다이아 사용기간 변경

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -8,6 +8,16 @@ class Schedule_model extends CI_Model {
         parent::__construct();
     }
     
+    public function get_schedule_cnt($country, $year , $month){
+       
+        $sSql = "SELECT count(*) as cnt,DATE_FORMAT(ki.start_date,'%Y-%m-%d') AS start_date,DATE_FORMAT(ki.end_date,'%Y-%m-%d') AS end_date FROM kpop_info AS ki 
+                 WHERE ki.start_date LIKE '$year-$month%' AND ki.country_idx ='$country' group by ki.start_date";
+        
+        $query = $this->db->query($sSql);
+        return $query->result_array();
+        
+    }
+    
     public function get_schedule($country, $year , $month){
        
         $sSql = "SELECT ki.name, DATE_FORMAT(ki.start_date,'%Y-%m-%d') AS start_date, DATE_FORMAT(ki.end_date,'%Y-%m-%d') AS end_date, ki.idx FROM kpop_info AS ki 
