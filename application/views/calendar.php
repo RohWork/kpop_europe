1e15c1e64e05b680f0bfb32c77df7a6b8db62c24
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-04 10:53:41 +0900

    페이지 수정

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -104,7 +104,7 @@
                         if($cnt > 0){ ?>
                         
                         <br/>
-                        <font style='font-size:10px;cursor:pointer' onclick="go_list('<?=$date?>')"><?=$cnt?></font>
+                        <div style="text-align: right"><font style='font-size:10px;cursor:pointer' onclick="go_list('<?=$date?>')"><?=$cnt?></font></div>
                         
                         <?php
                         }
