f6c55c667ac23e8aff1b4e0de3561c8bdd4eff93
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-04 10:42:41 +0900

    페이지 수정

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -10,7 +10,7 @@ class Schedule_model extends CI_Model {
     
     public function get_schedule_cnt($country, $year , $month){
        
-        $sSql = "SELECT count(*) as cnt,DATE_FORMAT(ki.start_date,'%Y-%m-%d') AS start_date,DATE_FORMAT(ki.end_date,'%Y-%m-%d') AS end_date FROM kpop_info AS ki 
+        $sSql = "SELECT count(*) as cnt,DATE_FORMAT(ki.start_date,'%d') AS start_date,DATE_FORMAT(ki.end_date,'%d') AS end_date FROM kpop_info AS ki 
                  WHERE ki.start_date LIKE '$year-$month%' AND ki.country_idx ='$country' group by ki.start_date";
         
         $query = $this->db->query($sSql);
