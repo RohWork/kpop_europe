094522c92673dbd1adb0662b8400998de3980bb4
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-04 12:34:39 +0900

    페이지 수정

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -18,10 +18,10 @@ class Schedule_model extends CI_Model {
         
     }
     
-    public function get_schedule($country, $year , $month){
+    public function get_schedule($country, $date){
        
         $sSql = "SELECT ki.name, DATE_FORMAT(ki.start_date,'%Y-%m-%d') AS start_date, DATE_FORMAT(ki.end_date,'%Y-%m-%d') AS end_date, ki.idx FROM kpop_info AS ki 
-                 WHERE ki.start_date LIKE '$year-$month%' AND ki.country_idx ='$country'";
+                 WHERE ki.start_date =< $date and ki.end_date >= $date AND ki.country_idx ='$country'";
         
         $query = $this->db->query($sSql);
         return $query->result_array();
