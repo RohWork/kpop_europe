3bf6eeb79dd248aca83d081ba9ce089ae6ca6380
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-04 10:57:00 +0900

    페이지 수정

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -122,7 +122,7 @@
     
     <div id="detail_modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
         
-        <div class="modal-dialog modal-sm" style="max-height:85%;" role="document">
+        <div class="modal-dialog modal-lg" style="max-height:85%;" role="document">
             <div class="modal-content" style="height:800px">
                 <div class="modal-header">
                     <h5 class="modal-title">detail</h5>
