9e929220995f135fa2fe846115c81001991b7a28
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-05 09:58:06 +0900

    페이지 수정

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -160,14 +160,7 @@
     
     function go_detail(idx, name){
      
-        var url = "/schedule/detail/"+idx;
-       
-        
-        $('#detail_frame').attr('src', url);
-        
-        $(".modal-title").text(name);
-        // 모달창 띄우기
-        $('#detail_modal').modal("show");
+        parent.go_detail(idx,name);
         
     }
     
