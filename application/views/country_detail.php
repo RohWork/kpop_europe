24d416a3076087ae5898f536be9d1a3afe0fd2f2
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-05 11:29:04 +0900

    페이지 수정

--- application/views/country_detail.php
+++ application/views/country_detail.php
@@ -39,7 +39,7 @@
                 },
                 error: function(xhr,status,error) {
                     console.log(xhr,status,error);
-                    alert("네트워크 오류!! 관리자에게 문의 주세요!!");
+                    alert("Network error!! Confirm to Manager!!");
                     return false;
                 }	 
             });
@@ -66,7 +66,7 @@
                     },
                     error: function(xhr,status,error) {
                         console.log(xhr,status,error);
-                        alert("네트워크 오류!! 관리자에게 문의 주세요!!");
+                        alert("Network error!! Confirm to Manager!!");
                         return false;
                     }	 
                 });
