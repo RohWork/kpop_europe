9154803899993b1f349439b9826ebcbaa146ccb7
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-06 10:11:54 +0900

    페이지 수정

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -30,7 +30,7 @@ class Schedule_model extends CI_Model {
         }
         
         $sSql = "SELECT  ki.name, DATE_FORMAT(ki.start_date,'%Y-%m-%d') AS start_date, DATE_FORMAT(ki.end_date,'%Y-%m-%d') AS end_date, ki.idx FROM kpop_info AS ki 
-                 WHERE 1=1 ".$where
+                 WHERE 1=1 ".$where;
         
         
         $query = $this->db->query($sSql);
