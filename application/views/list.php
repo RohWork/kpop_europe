e869ee17fe231a238693c7ff453ddbc9113421ba
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-06 10:56:24 +0900

    페이지 수정

--- application/views/list.php
+++ application/views/list.php
@@ -13,7 +13,7 @@
                             $i=1;
                             foreach($list as $li){
                         ?>        
-                        <tr onclick="go_detail(<?=$li['idx']?>,'<?=$li['name']?>')" style="cursor: pointer">
+                        <tr onclick="go_detail(<?=$li['idx']?>)" style="cursor: pointer">
                             <td><?=$i?></td>
                             <td><?=$li['name']?></td>
                             <td><?=$li['start_date']?></td>
@@ -28,36 +28,15 @@
             </div>
         </div>
         
-        
-        
-        <div id="detail_modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
-
-            <div class="modal-dialog modal-lg" style="max-height:85%;" role="document">
-                <div class="modal-content" style="height:600px">
-                    <div class="modal-header">
-                        <h5 class="modal-title">detail</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <iframe src="" id="detail_frame" style="width:100%; height:100%">etc</iframe>  
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary close" data-dismiss="modal" aria-label="Close">Close</button>
-                        <button type="button" class="btn btn-secondary list" aria-label="List">List</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
     <script>
     
     function go_detail(idx){
 
-        location.href="/schedule/detail/"+idx;
+        var url="/schedule/detail/"+idx;
+        
+        window.open(url);
         
     }
-
+    
     </script>
 </main>
