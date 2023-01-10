553503bbe971dd9992a200df967c5d9d9c38a08e
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-10 10:23:43 +0900

    페이지 수정

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -18,18 +18,23 @@ class Schedule_model extends CI_Model {
         
     }
     
-    public function get_schedule($country="", $date=""){
+    public function get_schedule($country="", $date="", $paging=""){
        
         $where = "";
+        $limit = "";
+        
         if(!empty($country)){
             $where .= " AND ki.country_idx ='$country'";
         }
         if(!empty($date)){
             $where .= " and DATE_FORMAT(ki.start_date,'%Y-%m-%d') <= '$date' and DATE_FORMAT(ki.end_date,'%Y-%m-%d') >= '$date'";
         }
+        if(!empty($paging)){
+            $limit = "limit ".$paging['start'].",".$paging['end'];
+        }
         
         $sSql = "SELECT  ki.name, DATE_FORMAT(ki.start_date,'%Y-%m-%d') AS start_date, DATE_FORMAT(ki.end_date,'%Y-%m-%d') AS end_date, ki.idx FROM kpop_info AS ki 
-                 WHERE 1=1 ".$where." order by ki.end_date desc";
+                 WHERE 1=1 ".$where." order by ki.end_date desc $limit";
         
         $query = $this->db->query($sSql);
         return $query->result_array();
