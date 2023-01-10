42841732c3dd9ca5f655d498b1eac367b5926f59
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-10 09:56:53 +0900

    페이지 수정

--- application/views/detail.php
+++ application/views/detail.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
             .row_border{
-                border-bottom: 1px solid #CCCCCC;
+                border-bottom: 1px solid #CCCCCC; height: 30px;
             }
         </style>
     </head>
