4ebb21c49d69de30e013c0c3ea7ccd6cc51bf766
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-13 08:12:43 +0900

    페이지 수정

---
+++ application/views/modify.php
@@ -0,0 +1,47 @@
+
+        <div class="container" style="font-size: 12px;max-width:100%">
+            <div class="row">
+                <div class="col-12">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>START_DATE</th>
+                            <th>END_DATE</th>
+                        </tr>
+                        <?php 
+                            $i=1;
+                            foreach($list as $li){
+                        ?>        
+                        <tr onclick="go_detail(<?=$li['idx']?>)" style="cursor: pointer">
+                            <td><?=$i?></td>
+                            <td><?=$li['name']?></td>
+                            <td><?=$li['start_date']?></td>
+                            <td><?=$li['end_date']?></td>
+                        </tr>   
+                        <?php 
+                            $i++;
+                            }
+                        ?>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center">
+                    <?=$paging?>
+                </div>
+            </div>
+        </div>
+        
+    <script>
+    
+    function go_detail(idx){
+
+        var url="/schedule/detail/"+idx;
+        
+        window.open(url, 'detail', "width=500, height=700" );
+        
+    }
+    
+    </script>
+</main>
