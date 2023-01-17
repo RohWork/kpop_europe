8e85b1d2b71fedde95fa118b317a5c5268715dcc
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-17 09:19:30 +0900

    페이지 수정

--- application/views/modify.php
+++ application/views/modify.php
@@ -111,13 +111,13 @@
 
                 <?php if($this->session->userdata('level') > 2 || $this->session->userdata('org_idx') == $detail_info['organization_idx']){ ?>
                 <div class="row">
-                    <div class="col-4 text-center">
+                    <div class="col-2 col-offset-2 text-center">
                         <button type="button" onclick="set_modify()" class="btn btn-warning">SUBMIT</button>
                     </div>
-                    <div class="col-4 text-center">
+                    <div class="col-2 text-center">
                         <button type="button" onclick="form_reset()" class="btn btn-info">RESET</button>
                     </div>
-                    <div class="col-4 text-center">
+                    <div class="col-2 text-center">
                         <button type="button" onclick="go_return()" class="btn btn-info">CANCEL</button>
                     </div>
                 </div>
