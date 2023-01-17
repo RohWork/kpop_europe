7ca7594b39516bc34feb7d3b3da2213f1f96d22a
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-17 09:27:04 +0900

    페이지 수정

--- application/views/modify.php
+++ application/views/modify.php
@@ -101,7 +101,7 @@
                     <div class="col-3">
                         <label class="form-label"><strong>Image</strong></label>
                     </div>
-                    <div class="col-9" id="image_group" style="overflow-y: auto;height: 100px>
+                    <div class="col-9" id="image_group" style="overflow-y: auto;height: 100px">
                         <div class="input-group mb-2 mt-1">
                             <input type="text" id="input_image[]" class="form-control i_img" name="input_image[]"/>
                             <button type="button" class="btn btn-primary" id="input_url">+</button>
