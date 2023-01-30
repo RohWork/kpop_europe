c3368d175c1fcd35ceeb7b80a95dc69f6876a81f
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-30 10:40:51 +0900

    페이지 수정

--- application/views/schedule_insert.php
+++ application/views/schedule_insert.php
@@ -220,8 +220,8 @@
         });
         
     }
-    $('#input_end_date') .datepicker();
-    $('#input_start_date') .datepicker();
+    $('#input_end_date') .datepicker({});
+    $('#input_start_date') .datepicker({});
     </script>
 </main>
 
