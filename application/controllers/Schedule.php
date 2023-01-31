257b64686252d30d48f43c9729ad7650a134e94e
ict_team <ict_team@DESKTOP-M7D75CU>
2023-01-31 09:23:46 +0900

    페이지 수정

--- application/controllers/Schedule.php
+++ application/controllers/Schedule.php
@@ -76,9 +76,9 @@ class Schedule extends CI_Controller {
         
         $search = array();
         
-        
-        $search['date'] =  date("Y-m-d", strtotime($this->input->get_post("date")));
-        
+        if(!empty($this->input->get_post("date"))){
+            $search['date'] =  date("Y-m-d", strtotime($this->input->get_post("date")));
+        }
         $search['country'] = $this->input->get_post("country");
         $search['city'] = $this->input->get_post("city");
         $search['organizer'] = $this->input->get_post("organizer");
