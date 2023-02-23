dde859b9199b14f1524481185a693faa68de87b9
ict_team <ict_team@DESKTOP-M7D75CU>
2023-02-24 08:46:42 +0900

    리딩매직 개발 by노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -7,25 +7,89 @@
 
     <div class="container">
         
-        <div class="row">
-            <div class="col-2">
-                <select id="country" name="country" class="form-select">
-                    <?php
-                        foreach($country as $cont){
-                            if($cont['idx'] == $get_country){
-                                $selected = "selected";
-                            }else{
-                                $selected = "";
-                            }
-                    ?>
-                    <option value="<?=$cont['idx']?>" <?=$selected?>><?=$cont['name']?></option>
-                    <?php
-                        }
-                    ?>
-                </select>
-                
+        <form method="get">
+            <div class="row">
+                <div class="col-2">
+                    <div class="form-floating">
+                        <select id="check_country" name="country" class="form-select" onchange="get_country_data()">
+                            <option value=""></option>
+                            <?php foreach($country_list as $cnt){ 
+                                $search_cnt = "";
+                                if($search['country'] == $cnt['idx']){
+                                    $search_cnt = "selected";
+                                }
+                            ?>
+                                <option value="<?=$cnt['idx']?>" <?=$search_cnt?>><?=$cnt['name']?></option>
+
+                            <?php } ?>
+                        </select>
+                        <label class="form-label col-1">
+                            COUNTRY
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-2">
+                    <div class="form-floating">
+                        <select id="check_city" name="city" class="form-select">
+                            <option value=""></option>
+                            <?php foreach($city_list as $cty){ 
+
+                                $search_cty = "";
+                                if($search['city'] == $cty['idx']){
+                                    $search_cty = "selected";
+                                }
+
+                            ?>
+                                <option value="<?=$cty['idx']?>" <?=$search_cty?>><?=$cty['name']?></option>
+
+                            <?php } ?>
+                        </select>
+                        <label class="form-label col-1">
+                            CITY
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-2">
+                    <div class="form-floating">
+                        <select id="organization" name="organization" class="form-select">
+                            <option value=""></option>
+                            <?php foreach($organization_list as $org){ 
+
+                                $search_org = "";
+                                if($search['organizer'] == $org['idx']){
+                                    $search_org = "selected";
+                                }
+
+                            ?>
+                                <option value="<?=$org['idx']?>" <?=$search_org?>><?=$org['name']?></option>
+
+                            <?php } ?>
+                        </select>
+                        <label for="organization">
+                            ORGERNIZER
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-2">
+                    <div class="form-floating">
+                        <input type="text" id="date" name="date" class="form-control" value="<?=$search['date']?>"/>
+                        <label for="date">
+                            SEARCH DATE
+                        </label>
+                    </div>
+                </div>
+
+
+                <div class="col-1">
+
+                    <input type="submit" value="SEARCH" class="btn btn-success"/>
+                </div>
+
             </div>
-        </div>
+        </form>
         
         <div class="row">
             <div class="col-12">
