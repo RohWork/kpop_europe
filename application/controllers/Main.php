3681c74dc6428a3bdb459a12578f326088113018
ict_team <ict_team@DESKTOP-M7D75CU>
2023-02-23 16:12:16 +0900

    리딩매직 개발 by노진국

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -12,10 +12,59 @@ class Main extends CI_Controller {
         $this->load->model('Organization_model', 'org_md', TRUE);
     }
     function index(){
+        
+        $data['get_country'] = $country = $this->input->get('country') !== null ? $this->input->get('country') : '1';
+        
+        //---- 오늘 날짜
+        $data['thisyear'] = $thisyear = date('Y'); // 4자리 연도
+        $data['thismonth'] = $thismonth = date('n'); // 0을 포함하지 않는 월
+        $data['today'] = $today = date('j'); // 0을 포함하지 않는 일
+
+        //------ $year, $month 값이 없으면 현재 날짜
+        $data['year'] = $year = $this->input->get('year') !== null ? $this->input->get('year') : $thisyear;
+        $data['month'] =$month =  $this->input->get('month') !== null ?$this->input->get('month') : $thismonth;
+        $data['day'] = $day = $this->input->get('month') !== null ? $this->input->get('month') : $today;
+        
+        $data['country'] = $this->cont_md->get_country(); 
+        
+        $data_calendar = $this->sch_md->get_schedule_cnt($country, $year , sprintf("%02d",$month));
+
+        $data['calendar'] = $data_calendar;
+        
+        
+        $data['prev_month'] = $prev_month = $month - 1;
+        $data['next_month'] = $next_month =  $month + 1;
+        $data['prev_year'] = $data['next_year'] = $year;
+        
+        if ($month == 1) {
+            $data['prev_month'] = 12;
+            $data['prev_year'] = $year - 1;
+        } else if ($month == 12) {
+            $data['next_month'] = 1;
+            $data['next_year'] = $year + 1;
+        }
+        $data['preyear'] = $year - 1;
+        $data['nextyear'] = $year + 1;
+
+        $data['predate'] = date("Y-m-d", mktime(0, 0, 0, $month - 1, 1, $year));
+        $data['nextdate'] = date("Y-m-d", mktime(0, 0, 0, $month + 1, 1, $year));
+
+        // 1. 총일수 구하기
+        $data['max_day'] = $max_day =  date('t', mktime(0, 0, 0, $month, 1, $year)); // 해당월의 마지막 날짜
+        //echo '총요일수'.$max_day.'<br />';
+
+        // 2. 시작요일 구하기
+        $data['start_week'] = $start_week =  date("w", mktime(0, 0, 0, $month, 1, $year)); // 일요일 0, 토요일 6
+
+        // 3. 총 몇 주인지 구하기
+        $data['total_week'] = ceil(($max_day + $start_week) / 7);
+
+        // 4. 마지막 요일 구하기
+        $data['last_week'] = date('w', mktime(0, 0, 0, $month, $max_day, $year));
 
         if(confirm_mobile()){ //MOBILE
             $this->load->view('/mobile/header');
-
+            $this->load->view('calendar',$data);
             $this->load->view('footer');
         }else{                                                      //PC
             $this->load->view('header');
