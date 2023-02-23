c7e9cedc276946bab3b339ea99978427b1c58f67
ict_team <ict_team@DESKTOP-M7D75CU>
2023-02-23 16:09:24 +0900

    리딩매직 개발 by노진국

---
+++ application/helpers/common_helper.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+function confirm_mobile(){
+    
+    $mobile_agent = "/(iPod|iPhone|Android|BlackBerry|SymbianOS|SCH-M\d+|Opera Mini|Windows CE|Nokia|SonyEricsson|webOS|PalmOS)/";
+
+        if(preg_match($mobile_agent, $_SERVER['HTTP_USER_AGENT'])){
+            return true;
+        }else{
+            return false;
+        }
+    
+}
+
+
+?>
