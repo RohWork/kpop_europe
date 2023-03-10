845787fedb26a13e30ca9cd51e3a65f1b391a0b7
ict_team <ict_team@DESKTOP-M7D75CU>
2023-03-10 10:30:16 +0900

    리딩매직 사용자관리 페이지 추가

--- application/models/City_model.php
+++ application/models/City_model.php
@@ -29,7 +29,7 @@ class City_model extends CI_Model {
 
         $sSql = "SELECT kc.*, ko.name as country_name FROM `kpop_city` as kc "
                 . "left join kpop_country as ko on kc.country_idx = ko.idx "
-                . "where kc.state = 1 $where order by kc.order, kc.country_idx asc, ko.idx desc";
+                . "where kc.state = 1 $where order by kc.ord, kc.country_idx asc, ko.idx desc";
         
         $query = $this->db->query($sSql);
         return $query->result_array();
