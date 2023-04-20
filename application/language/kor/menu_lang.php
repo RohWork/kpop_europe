7e4acf74247b98f231b7c725e2a60135e69f9fc0
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-20 10:42:47 +0900

    리딩매직 사용자관리 페이지 추가

---
+++ application/language/kor/menu_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+    $lang['kpop_schedule'] = 'kpop 일정';
+    $lang['schedule'] = '달력확인';
+    $lang['community'] = '커뮤니티';
+    $lang['schedule'] = '일정 리스트';
+    $lang['data_insert'] = '데이터 추가';
+    $lang['excel_upload'] = '엑셀 입력';
+    $lang['Management'] = '관리';
+    $lang['Country'] = '국가';
+    $lang['City'] = '도시';
+    $lang['Organizer'] = '사업자';
+    $lang['toadd'] = '이벤트 스케줄을 추가하고, 수정하세요.';
+    $lang['contact'] = '문의하기';
+?>
