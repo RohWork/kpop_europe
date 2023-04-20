6f41d99f001157781fe3ada87929904066944c5a
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-20 11:16:47 +0900

    리딩매직 사용자관리 페이지 추가

--- application/language/kor/menu_lang.php
+++ application/language/kor/menu_lang.php
@@ -7,9 +7,9 @@
     $lang['data_insert'] = '데이터 추가';
     $lang['excel_upload'] = '엑셀 입력';
     $lang['Management'] = '관리';
-    $lang['Country'] = '국가';
-    $lang['City'] = '도시';
-    $lang['Organizer'] = '사업자';
+    $lang['Country'] = '국가 관리';
+    $lang['City'] = '도시 관리';
+    $lang['Organizer'] = '사업자 관리';
     $lang['toadd'] = '이벤트 스케줄을 추가하고, 수정하세요.';
     $lang['contact'] = '문의하기';
 ?>
