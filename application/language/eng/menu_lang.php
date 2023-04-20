c94d880a5544293b4138a457634d81dd1b6d2c83
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-20 10:42:49 +0900

    리딩매직 사용자관리 페이지 추가

---
+++ application/language/eng/menu_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+    $lang['kpop_schedule'] = 'Kpop Schedule';
+    $lang['schedule'] = 'Schedule';
+    $lang['community'] = 'Community';
+    $lang['List'] = 'List';
+    $lang['data_insert'] = 'Data Insert';
+    $lang['excel_upload'] = 'Excel Upload';
+    $lang['Management'] = 'Management';
+    $lang['Country'] = 'Country';
+    $lang['City'] = 'City';
+    $lang['Organizer'] = 'Organizer';
+    $lang['toadd'] = 'to add,modify your schedule';
+    $lang['contact'] = 'Contact us';
+?>
