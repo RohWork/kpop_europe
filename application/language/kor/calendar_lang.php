8555c1a1a34f30f8c576782893379577ba3f7399
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-20 13:24:04 +0900

    리딩매직 사용자관리 페이지 추가

---
+++ application/language/kor/calendar_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+    $lang['type'] = '타입';
+    $lang['country'] = '국가';
+    $lang['city'] = '도시';
+    $lang['orgernizer'] = '사업자';
+    $lang['search'] = '검색';
+    $lang['sun'] = '일';
+    $lang['mon'] = '월';
+    $lang['tue'] = '화';
+    $lang['wed'] = '수';
+    $lang['thu'] = '목';
+    $lang['fri'] = '금';
+    $lang['sat'] = '토';
+    $lang['count'] = '갯수';
+    
+?>
