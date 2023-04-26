797275e6a62f24c782e8c267c9caea40ab018ccf
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-26 10:21:28 +0900

    리딩매직 사용자관리 페이지 추가

--- application/language/eng/controller_lang.php
+++ application/language/eng/controller_lang.php
@@ -1,7 +1,41 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+    $lang['type'] = 'TYPE';
+    $lang['country'] = 'COUNTRY';
+    $lang['city'] = 'CITY';
+    $lang['orgernizer'] = 'ORGERNIZER';
+    $lang['search'] = 'SEARCH';
+    $lang['sun'] = 'SUN';
+    $lang['mon'] = 'MON';
+    $lang['tue'] = 'TUE';
+    $lang['wed'] = 'WED';
+    $lang['thu'] = 'THU';
+    $lang['fri'] = 'FRI';
+    $lang['sat'] = 'SAT';
+    $lang['count'] = 'COUNT';
+    $lang['detail'] = 'DETAIL';
+    $lang['hi'] = "hello";
+    
+    $lang['neterror'] = 'Network Error!! take support to web manager!!';
+    $lang['dataerror'] = 'data process error';
+    $lang['dataerror2'] = "Data processing failure";
+    $lang['idxerror'] = "Check To you're idx";
+    $lang['mailerror'] = "Duplicate email. Check to your email adress.";
+    $lang['mailerror2'] = "this E-mail is not confirmed. Take to E-mail confirm";
+    $lang['dupleschedule'] = "Duplicated schedule data.";
+    $lang['mineerror'] = "Not You'r Schedule";
+    $lang['paramerror'] = "Check To you're name or start_date or end_date";
+    $lang['orgerror'] = "Not found Organizer.";
+    $lang['countryerror'] = "Not found Country.";
+    $lang['cityerror'] = "Not found City.";
+    
+    
+    $lang['checkdate'] = "Check To you're name or start_date or end_date"; 
+    $lang['checkid'] = "Check To you're ID or PASSWORD";
+    
+    
+    $lang['confirm'] = "Confirm to your address";
+    $lang['visit'] = "visit to :";
+    $lang['url'] = "this url";
+    
+?>
