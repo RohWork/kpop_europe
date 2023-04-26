1067d0629882b29f9952aa2733c3d643f7b0fd01
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-26 11:28:12 +0900

    리딩매직 사용자관리 페이지 추가

--- application/language/eng/view_lang.php
+++ application/language/eng/view_lang.php
@@ -1,7 +1,86 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+    $lang['type'] = 'TYPE';
+    $lang['country'] = 'COUNTRY';
+    $lang['city'] = 'CITY';
+    $lang['orgernizer'] = 'ORGERNIZER';
+    
+    $lang['location'] = "Location";
+    $lang['address'] = "Address";
+    $lang['joinus'] = "Join Us";
+    $lang['gojoin'] = "Go join";
+    $lang['party'] = "PARTY";
+    $lang['concert'] = "CONCERT";
+    $lang['findpw'] = "Find Password";
 
+    
+    
+    $lang['sun'] = 'SUN';
+    $lang['mon'] = 'MON';
+    $lang['tue'] = 'TUE';
+    $lang['wed'] = 'WED';
+    $lang['thu'] = 'THU';
+    $lang['fri'] = 'FRI';
+    $lang['sat'] = 'SAT';
+    $lang['count'] = 'COUNT';
+    $lang['startdate'] = "START DATE";
+    $lang['enddate'] = "END DATE";
+    $lang['searchdate'] = "SEARCH DATE";
+    
+    $lang['detail'] = 'DETAIL';
+    $lang['insert'] = "INSERT";
+    $lang['delete'] = "DELETE";
+    $lang['modify'] = "MODIFY";
+    $lang['cancel'] = "CANCEL";
+    $lang['list'] = "LIST";
+    $lang['search'] = 'SEARCH';
+    $lang['login'] = 'LOGIN';
+    
+    $lang['home'] = "HOME";
+    $lang['event'] = "Event";
+    $lang['home'] = "HOME";
+    $lang['date'] = "DATE";
+    $lang['etc'] = "Etc";
+    $lang['image'] = "Image";
+    $lang['previous'] = "Previous";
+    $lang['next'] = "Next";
+    $lang['password'] = "Password";
+    $lang['gname'] = "Given Name";
+    $lang['fname'] = "Family Name";
+    
+    
+    $lang['save'] = "SAVE";
+    $lang['close'] = "CLOSE";
+    $lang['reset'] = "RESET";
+    $lang['order'] = "Order";
+    $lang['name'] = "Name";
+    $lang['writer'] = "Writer";
+    $lang['registdate'] = "Regist date";
+    $lang['orgernizationname'] = "Organization name";
+    $lang['orgernizationorder'] = "Organization Order";
+    
+    $lang['neterror'] = 'Network Error!! take support to web manager!!';
+    $lang['countryerror'] = "insert to country data";
+    $lang['permiterror'] = "this not permmited";
+    $lang['mailerror'] = "Duplicate email. Check to your email adress";
+    $lang['confirmdata'] = "Take Confirm to your data.";
+    $lang['confirmemail'] = "Take Confirm to your Email address.";
+    $lang['confirmpassword'] = "check to your Password";
+    $lang['loginfail'] = "login fail, retry to login";
+    
+     
+    $lang['cityname'] = "CITY NAME";
+    $lang['cityorder'] = "City Order";
+    $lang['countryname'] = "Country name";
+    $lang['completemodify'] = "Complete to modify";
+    $lang['completeinsert'] = "Complete to insert";
+    $lang['completedelete'] = "Complete to delete";
+    $lang['completeemail'] = "Complete to Confirm E-mail";
+    
+    $lang['checktodata.'] = "input fail. check to data.";
+    $lang['deletecity'] = "do you want to delete city?";
+    $lang['completedelete'] = "complete to delete";
+    $lang['checkemail'] = "Check to Your E-mail!";
+    
+    
+?>
