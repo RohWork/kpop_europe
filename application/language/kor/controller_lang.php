b34e0df190e0f7ed9d3c790cf59c2d6a8e80915b
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-26 13:11:34 +0900

    리딩매직 사용자관리 페이지 추가

--- application/language/kor/controller_lang.php
+++ application/language/kor/controller_lang.php
@@ -1,7 +1,41 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+    $lang['type'] = '타입';
+    $lang['country'] = '국가';
+    $lang['city'] = '도시';
+    $lang['orgernizer'] = '사업자';
+    $lang['search'] = '검색';
+    $lang['sun'] = '일';
+    $lang['mon'] = '월';
+    $lang['tue'] = '화';
+    $lang['wed'] = '수';
+    $lang['thu'] = '목';
+    $lang['fri'] = '금';
+    $lang['sat'] = '토';
+    $lang['count'] = '갯수';
+    $lang['detail'] = '상세';
+    $lang['hi'] = "안녕하세요";
+    
+    $lang['neterror'] = '네트워크 에러!! 웹관리자에게 문의 하세요!!';
+    $lang['dataerror'] = '데이터 처리 오류';
+    $lang['dataerror2'] = "데이터 처리가 실패했습니다.";
+    $lang['idxerror'] = "인덱스 번호를 확인하세요.";
+    $lang['mailerror'] = "중복된 이메일. 이메일 주소를 확인하시오.";
+    $lang['mailerror2'] = "이메일이 확인되지 않았습니다. 이메일 확인을 실행하세요.";
+    $lang['dupleschedule'] = "일정 데이터가 중복 되었습니다..";
+    $lang['mineerror'] = "당신의 스케쥴 데이터가 아닙니다.";
+    $lang['paramerror'] = "이름, 시작일과 종료일 데이터를 확인하세요.";
+    $lang['orgerror'] = "사업자가 존재하지 않습니다.";
+    $lang['countryerror'] = "국가데이터가 존재하지 않습니다.";
+    $lang['cityerror'] = "도시 데이터가 존재하지 않습니다.";
+    
+    
+    $lang['checkdate'] = "이름, 시작일과 종료일 데이터를 확인하세요."; 
+    $lang['checkid'] = "아이디와 패스워드를 확인하시오";
+    
+    
+    $lang['confirm'] = "주소를 확인하세요.";
+    $lang['visit'] = "링크 확인요망 :";
+    $lang['url'] = "해당 URL을 확인하세요.";
+    
+?>
