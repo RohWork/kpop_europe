5c1107f5eaaf931bc73b044c92fd67d4c7cff22f
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-26 13:26:09 +0900

    리딩매직 사용자관리 페이지 추가

--- application/language/kor/view_lang.php
+++ application/language/kor/view_lang.php
@@ -1,7 +1,91 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+    $lang['type'] = '타입';
+    $lang['country'] = '국가';
+    $lang['city'] = '도시';
+    $lang['orgernizer'] = '주최사';
+    
+    $lang['location'] = "위치";
+    $lang['address'] = "주소";
+    $lang['joinus'] = "회원가입";
+    $lang['gojoin'] = "가입";
+    $lang['party'] = "파티";
+    $lang['concert'] = "콘서트";
+    $lang['findpw'] = "비밀번호찾기";
 
+    
+    
+    $lang['sun'] = '일';
+    $lang['mon'] = '월';
+    $lang['tue'] = '화';
+    $lang['wed'] = '수';
+    $lang['thu'] = '목';
+    $lang['fri'] = '금';
+    $lang['sat'] = '토';
+    $lang['count'] = '갯수';
+    
+    $lang['startdate'] = "시작일";
+    $lang['enddate'] = "종료일";
+    $lang['searchdate'] = "검색 날짜";
+    
+    $lang['detail'] = "상세보기";
+    $lang['insert'] = "추가";
+    $lang['delete'] = "삭제";
+    $lang['modify'] = "수정";
+    $lang['cancel'] = "취소";
+    $lang['list'] = "목록";
+    $lang['search'] = "검색";
+    $lang['login'] = "로그인";
+    $lang['save'] = "저장";
+    $lang['close'] = "닫기";
+    $lang['reset'] = "초기화";
+    
+    $lang['home'] = "HOME";
+    $lang['event'] = "Event";
+    $lang['date'] = "날짜";
+    $lang['etc'] = "기타";
+    $lang['image'] = "Image";
+    $lang['previous'] = "이전";
+    $lang['next'] = "다음";
+    $lang['password'] = "비밀번호";
+    $lang['gname'] = "이름";
+    $lang['fname'] = "성";
+    
+   
+    $lang['order'] = "순서";
+    $lang['name'] = "이름";
+    $lang['writer'] = "작성자";
+    $lang['registdate'] = "등록일";
+    $lang['orgernizationname'] = "주최사 명";
+    $lang['orgernizationorder'] = "주최사 순서";
+    $lang['orgernizationinert'] = "주최사 추가";
+    
+    
+    $lang['neterror'] = '네트워크 에러!! 웹관리자에게 문의 하세요!!';
+    $lang['countryerror'] = "국가 데이터를 입력하세요.";
+    $lang['permiterror'] = "허용되지 않았습니다.";
+    $lang['mailerror'] = "중복된 이메일. 이메일 주소를 확인하시오.";
+    $lang['mailerror2'] = "이메일이 확인되지 않았습니다. 이메일 확인을 실행하세요.";
+    $lang['confirmdata'] = "입력된 데이터를 확인하세요..";
+    $lang['confirmemail'] = "이메일주소를 확인하세요.";
+    $lang['confirmpassword'] = "비밀번호를 확인하세요.";
+    $lang['loginfail'] = "로그인실패, 다시 로그인을 진행하세요.";
+    
+     
+    $lang['eventtype'] = "이벤트 타입";
+    
+    $lang['cityname'] = "도시 명";
+    $lang['cityorder'] = "도시 순서";
+    $lang['countryname'] = "국가 명";
+    $lang['completemodify'] = "수정 완료";
+    $lang['completeinsert'] = "입력 완료";
+    $lang['completedelete'] = "삭제 완료";
+    $lang['completeemail'] = "이메일 확인완료";
+    
+    $lang['checktodata.'] = "입력된 데이터를 확인 하세요.";
+    $lang['deletecity'] = "도시 데이터를 삭제하시겠습니까??";
+    $lang['deleteorganization'] = "주최사 데이터를 삭제 하시겠습니까?";
+    $lang['checkemail'] = "이메일을 확인하세요!";
+    
+    
+?>
