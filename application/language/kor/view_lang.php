0ffd23d428ebc9696771903e93384d49310bfc63
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-28 09:59:14 +0900

    리딩매직 사용자관리 페이지 추가

--- application/language/kor/view_lang.php
+++ application/language/kor/view_lang.php
@@ -39,6 +39,7 @@
     $lang['save'] = "저장";
     $lang['close'] = "닫기";
     $lang['reset'] = "초기화";
+    $lang['searchdate'] = "검색일자";
     
     $lang['home'] = "HOME";
     $lang['event'] = "Event";
@@ -50,7 +51,7 @@
     $lang['password'] = "비밀번호";
     $lang['gname'] = "이름";
     $lang['fname'] = "성";
-    
+    $lang['confirm'] = "확인";
    
     $lang['order'] = "순서";
     $lang['name'] = "이름";
@@ -58,7 +59,8 @@
     $lang['registdate'] = "등록일";
     $lang['orgernizationname'] = "주최사 명";
     $lang['orgernizationorder'] = "주최사 순서";
-    $lang['orgernizationinert'] = "주최사 추가";
+    $lang['orgernizationinsert'] = "주최사 추가";
+    $lang['excelupload'] = "엑셀 데이터 입력.";
     
     
     $lang['neterror'] = '네트워크 에러!! 웹관리자에게 문의 하세요!!';
@@ -66,13 +68,16 @@
     $lang['permiterror'] = "허용되지 않았습니다.";
     $lang['mailerror'] = "중복된 이메일. 이메일 주소를 확인하시오.";
     $lang['mailerror2'] = "이메일이 확인되지 않았습니다. 이메일 확인을 실행하세요.";
+    $lang['errorinsertcity'] = "도시정보 데이터를 추가하세요.";
+    $lang['errorinsertcountry'] = "국가정보 데이터를 추가하세요.";
     $lang['confirmdata'] = "입력된 데이터를 확인하세요..";
     $lang['confirmemail'] = "이메일주소를 확인하세요.";
     $lang['confirmpassword'] = "비밀번호를 확인하세요.";
     $lang['loginfail'] = "로그인실패, 다시 로그인을 진행하세요.";
-    
+    $lang['titleerror'] = "타이틀 정보를 입력하세요.";
      
     $lang['eventtype'] = "이벤트 타입";
+    $lang['contactus'] = "상담하기";
     
     $lang['cityname'] = "도시 명";
     $lang['cityorder'] = "도시 순서";
@@ -83,8 +88,10 @@
     $lang['completeemail'] = "이메일 확인완료";
     
     $lang['checktodata'] = "입력된 데이터를 확인 하세요.";
-    $lang['deletecity'] = "도시 데이터를 삭제하시겠습니까??";
+    $lang['deletecity'] = "도시 데이터를 삭제하시겠습니까?";
+    $lang['deletecountry'] = "국가 데이터를 삭제하시겠습니까?";
     $lang['deleteorganization'] = "주최사 데이터를 삭제 하시겠습니까?";
+    $lang['deleteschedule'] = "스케줄을 삭제 하시겠습니까?";
     $lang['checkemail'] = "이메일을 확인하세요!";
     
     
