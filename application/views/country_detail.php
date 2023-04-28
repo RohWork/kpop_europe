d1299e656a002230f131526d2cce33d5fded161b
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-28 13:53:46 +0900

    리딩매직 사용자관리 페이지 추가

--- application/views/country_detail.php
+++ application/views/country_detail.php
@@ -1,3 +1,10 @@
+
+<?php
+    $lang = $this->session->userdata('lang');
+    $this->lang->load('calendar', $lang);
+
+?>
+
 <html>
     <head>
         <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
