27ddbdb9616e53f86a43b5442b56d0a55655f2d3
ict_team <ict_team@DESKTOP-M7D75CU>
2023-04-28 13:09:44 +0900

    리딩매직 사용자관리 페이지 추가

--- application/views/detail.php
+++ application/views/detail.php
@@ -130,7 +130,7 @@
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden"><?=$this->lang->line('previus')?></span>
+                        <span class="visually-hidden"><?=$this->lang->line('previous')?></span>
                       </button>
                       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
