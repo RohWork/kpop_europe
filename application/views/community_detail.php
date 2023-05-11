38515cfdae46408282b4b3c1e5760727ad045be2
ict_team <ict_team@DESKTOP-M7D75CU>
2023-05-11 14:50:55 +0900

    리딩매직 사용자관리 페이지 추가

---
+++ application/views/community_detail.php
@@ -0,0 +1,108 @@
+<?php
+    $lang = $this->session->userdata('lang');
+    $this->lang->load('view', $lang);
+
+?>
+
+<html>
+    <head>
+        <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="container" style="font-size: 15px">
+            <form id="form_modify">
+                <div class="row">
+                    <div class="col-4">
+                        <label class="form-label bold"><strong><?=$this->lang->line('countryname')?></strong></label>
+                    </div>
+                    <div class="col-8">
+                        <select id="country_idx" name="country_idx"  class="form-select">
+                            <?php foreach($country as $cont){
+                                    $selected = "";
+                                    if($cont['idx'] == $detail_info['country_idx']){
+                                        $selected = "selected";
+                                    }
+                            ?>
+                                
+                                <option value="<?=$cont['idx']?>" <?=$selected?>><?=$cont['name']?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="row" style="padding-top: 5px">
+                    <div class="col-4">
+                        <label class="form-label bold"><strong><?=$this->lang->line('cityname')?></strong></label>
+                    </div>
+                    <div class="col-8">
+                        <input type="text" id="city" name="city" value="<?=$detail_info['name']?>" class="form-control"/>
+                        <input type="hidden" id="city_idx" name="city_idx" value="<?=$detail_info['idx']?>"/>
+                    </div>
+                </div>
+                <div class="row" style="padding-top: 5px">
+                    <div class="col-4">
+                        <label class="form-label bold"><strong><?=$this->lang->line('cityorder')?></strong></label>
+                    </div>
+                    <div class="col-8">
+                        <input type="number" id="order" name="order" value="<?=$detail_info['ord']?>" class="form-control"/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+    <script>
+        function modify_city(){
+            
+            $.ajax({
+                url:'/city/modify_ajax',
+                type:'post',
+                data:$("#form_modify").serialize(),
+                success:function(data){
+                    if(data.result == 200){
+                        alert('<?=$this->lang->line('completemodify')?>');
+                        window.parent.location.reload();
+                        window.parent.modal_close();
+                    }else{
+                        alert('<?=$this->lang->line('checktodata')?>');
+                    }
+                },
+                error: function(xhr,status,error) {
+                    console.log(xhr,status,error);
+                    alert("<?=$this->lang->line('neterror')?>");
+                    return false;
+                }	 
+            });
+            
+        }
+        function delete_city(){
+            
+            
+            if(confirm("<?=$this->lang->line('deletecountry')?>")){
+                var data = {idx : <?=$detail_info['idx']?>};
+
+                $.ajax({
+                    url:'/city/delete_ajax',
+                    type:'post',
+                    data:data,
+                    success:function(data){
+                        if(data.result == 200){
+                            alert('<?=$this->lang->line('completedelete')?>');
+                            window.parent.location.reload();
+                            window.parent.modal_close();
+                        }else{
+                            alert('<?=$this->lang->line('checktodata')?>');
+                        }
+                    },
+                    error: function(xhr,status,error) {
+                        console.log(xhr,status,error);
+                        alert("<?=$this->lang->line('neterror')?>");
+                        return false;
+                    }	 
+                });
+            }
+        }
+    </script>
+</html>
---
+++ application/views/community_write.php
@@ -0,0 +1,85 @@
+<?php
+    $lang = $this->session->userdata('lang');
+    $this->lang->load('view', $lang);
+
+?>
+
+    <div class="container">
+        <form id="city_insert">
+            <div class="form-group">
+                <div class="row">
+                    <label class="form-label"><h3><?=$this->lang->line('cityinsert')?></h3></label>
+                </div>
+                <div class="row mt-1">
+                    <label class="form-label col-md-2 col-xs-4"><strong><?=$this->lang->line('country')?></strong></label>
+
+                    <div class="col-md-4 col-xs-4 col-offset-6 col-xs-offset-4">
+                        <select id="check_country" name="check_country" class="form-select">
+                            <?php 
+                                foreach($country as $cnt){ 
+                                    $selected = "";
+                                    if($cnt['idx'] == $select_country){
+                                        $selected = "selected";
+                                    }
+                                    ?>
+                                
+                                <option value="<?=$cnt['idx']?>" <?=$selected?>><?=$cnt['name']?></option>
+
+                            <?php } ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row mt-1">
+                    <label class="form-label col-md-2 col-xs-4"><strong><?=$this->lang->line('cityname')?></strong></label>
+
+                    <div class="col-md-4 col-xs-4 col-md-offset-6 col-xs-offset-4">
+                        <input type="text" class="form-control" id="input_city" name="input_city"/>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-1" style="padding-top:50px">
+                
+                <div class="col-md-2 col-xs-2 "></div>
+                <div class="col-md-4 col-xs-6 col-offset-6 col-xs-offset-4 text-center">
+                    <button type="button" class="btn btn-success" id="btn_insert"><?=$this->lang->line('insert')?></button>
+                    <button type="button" class="btn btn-danger" id="btn_reset" onclick="form.reset();"><?=$this->lang->line('reset')?></button>
+                </div>
+            </div>
+        </form>
+        
+    </div>
+
+</main>
+<script>
+    $("#btn_insert").on('click', function(){
+        var city = $("#input_city").val();
+        if(city == ""){
+            alert("<?=$this->lang->line('errorinsertcity')?>");
+            return;
+        }else{
+            
+            $.ajax({
+                url:'/city/insert_ajax',
+                type:'post',
+                data:$("#city_insert").serialize(),
+                success:function(data){
+                    if(data.result == 200){
+                        alert('complete to insert');
+                        location.href = "/city?country=<?=$select_country?>";
+                    }else{
+                        alert('<?=$this->lang->line('checktodata')?>');
+                        console.log(data);
+                    }
+                },
+                error: function(xhr,status,error) {
+                    console.log(xhr,status,error);
+                    alert("<?=$this->lang->line('neterror')?>");
+                    return false;
+                }	 
+            });
+            
+        }
+    });
+    
+</script>
