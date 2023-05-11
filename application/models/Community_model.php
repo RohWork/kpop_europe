e70b066374259dded88ef695c97f81ac9ea5cd27
ict_team <ict_team@DESKTOP-M7D75CU>
2023-05-11 10:25:05 +0900

    리딩매직 사용자관리 페이지 추가

--- application/models/Community_model.php
+++ application/models/Community_model.php
@@ -8,5 +8,26 @@ class Community_model extends CI_Model {
         parent::__construct();
     }
     
-    
+    public function get_list($search, $paging){
+        
+        if(!empty($search['country'])){
+            $where .= " AND country_idx ='".$search['country']."'";
+        }
+        if(!empty($search['city'])){
+            $where .= " AND city_idx ='".$search['city']."'";
+        }
+        if(!empty($search['language'])){
+            $where .= " AND language ='".$search['language']."'";
+        }
+        if(!empty($paging)){
+            $limit = "limit ".$paging['start'].",".$paging['end'];
+        }
+        
+        $sSql = "select kc.* , 
+                (select count(*) from kpop_comment as kt where kt.community_idx = kc.idx ) as comment_idx 
+                from kpop_community as kc where 1=1 and $where $limit";
+        
+        $query = $this->db->query($sSql);
+        return $query->result_array();
+    }
 }
