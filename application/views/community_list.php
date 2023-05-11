057ba37b241bd64dbd43aa973fea7ee39803bc16
ict_team <ict_team@DESKTOP-M7D75CU>
2023-05-11 14:50:00 +0900

    리딩매직 사용자관리 페이지 추가

--- application/views/community_list.php
+++ application/views/community_list.php
@@ -116,7 +116,7 @@
             </div>
             <div class="row">
                 <div class="col-12 text-end">
-                    <button type="button" onclick="go_write" class="btn btn-primary"><?=$this->lang->line('write')?></button>
+                    <button type="button" onclick="go_write()" class="btn btn-primary"><?=$this->lang->line('write')?></button>
                 </div>
                 
             </div>
