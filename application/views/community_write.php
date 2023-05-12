776d32c974b52f5b29d434780ef22a2f78e7ca02
ict_team <ict_team@DESKTOP-M7D75CU>
2023-05-12 10:18:20 +0900

    리딩매직 사용자관리 페이지 추가

--- application/views/community_write.php
+++ application/views/community_write.php
@@ -114,7 +114,7 @@
 <script>
   ClassicEditor.create( document.querySelector( '#editor' ), {
       language : "<?=$lang?>",
-      extraPlugins: [MyCustomUploadAdapterPlugin],
+      extraPlugins: [MyCustomUploadAdapterPlugin]
   });
 </script>
 
