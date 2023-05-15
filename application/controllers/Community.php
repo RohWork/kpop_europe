8a3bd9c01d914936a583d1e7508bb79df836d7cc
ict_team <ict_team@DESKTOP-M7D75CU>
2023-05-15 10:20:09 +0900

    리딩매직 사용자관리 페이지 추가

--- application/controllers/Community.php
+++ application/controllers/Community.php
@@ -120,7 +120,8 @@ class Community extends CI_Controller {
         $data = array();
         
         $data['detail'] = $this->com_md->detail_community($idx);
-        $data['comment'] = $this->com_md->comment_community($idx);
+        $data['comment'] = $this->com_md->comment_community($idx, 1);
+        $data['sub_comment'] = $this->com_md->comment_community($idx, 1);
         $cnt = $this->com_md->count_community($idx);
         
         $this->load->view('header');
