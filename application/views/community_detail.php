9b9216dd4742309830aa05ab82cb660a6e778c2c
ict_team <ict_team@DESKTOP-M7D75CU>
2023-05-17 09:53:34 +0900

    리딩매직 사용자관리 페이지 추가

--- application/views/community_detail.php
+++ application/views/community_detail.php
@@ -18,10 +18,10 @@
                 
                 
                 <div class="row">
-                    <div class="col-1">
+                    <div class="col-2">
                         <label class="form-label bold"><strong><?=$this->lang->line('countryname')?></strong></label>
                     </div>
-                    <div class="col-2">
+                    <div class="col-1">
                         <?=$detail['country_name']?>
                     </div>
                     <div class="col-1">
