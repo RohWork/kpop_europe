4f690b7c1d46ae1582ee0c972dce5d074f67762f
ict_team <ict_team@DESKTOP-M7D75CU>
2023-05-19 08:51:33 +0900

    리딩매직 사용자관리 페이지 추가

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,8 @@
-<div class="container"> 
-    <footer class="py-3 my-4">
-        <p class="text-center text-muted">Copyright(C) 2022.Roh.All rights reserved</p>
-    </footer>
-</div>
+<footer>
+    <div class="container"> 
+        <footer class="py-3 my-4">
+            <p class="text-center text-muted">Copyright(C) 2022.Roh.All rights reserved</p>
+        </footer>
+    </div>
+</footer>
 </html>
