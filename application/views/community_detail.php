8c24a481b8ed1a6f5cfb41587ad7001d6a839c93
ict_team <ict_team@DESKTOP-M7D75CU>
2023-06-07 10:19:24 +0900

    닉네임 카드 서버 반영

--- application/views/community_detail.php
+++ application/views/community_detail.php
@@ -102,7 +102,7 @@
                                 <td><?=$cmt->content?></td>
                                 
                                 <td>
-                                    <button type="button" class="btn btn-success" onclick="like_comment(1, <?=$cmt->idx?>)"><?=$this->lang->line('like')?></button>
+                                    <button type="button" class="btn btn-success" onclick="like_comment(1, <?=$cmt->idx?>)"><?=$this->lang->line('like')?>( <?=$cmt->great?> ) </button>
                                 </td>
                                 <td>
                                     <button type="button" class="btn btn-secondary" onclick="like_comment(2, <?=$cmt->idx?>)"><?=$this->lang->line('dislike')?></button>
