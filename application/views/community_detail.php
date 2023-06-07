ae335013359e13fa4a2783309c6e6bc06bde8c90
ict_team <ict_team@DESKTOP-M7D75CU>
2023-06-07 09:58:29 +0900

    닉네임 카드 서버 반영

--- application/views/community_detail.php
+++ application/views/community_detail.php
@@ -54,8 +54,8 @@
                 
                 <div class="row" style="padding-top: 5px">
                     <div class="col-6">
-                        <button type="button" class="btn btn-success" onclick="like_community(1)"><?=$this->lang->line('like')?></button>
-                        <button type="button" class="btn btn-secondary" onclick="like_community(2)"><?=$this->lang->line('dislike')?></button>
+                        <button type="button" class="btn btn-success" onclick="like_community(1, <?=$idx?>)"><?=$this->lang->line('like')?></button>
+                        <button type="button" class="btn btn-secondary" onclick="like_community(2, <?=$idx?>)"><?=$this->lang->line('dislike')?></button>
                     </div>
                     <div class="col-3 text-end">
                         <?php if($this->session->userdata('id') == $detail['writer']){ ?>
@@ -94,7 +94,11 @@
                             ?>
                             <tr>
                                 <td><?=$cmt->content?></td>
-                                <td><?=$cmt->mnick?></td>
+                                <td>
+                                    <?=$cmt->mnick?>
+                                    <button type="button" class="btn btn-success" onclick="like_community(1, <?=$cmt->idx?>)"><?=$this->lang->line('like')?></button>
+                                    <button type="button" class="btn btn-secondary" onclick="like_community(2, <?=$cmt->idx?>)"><?=$this->lang->line('dislike')?></button>
+                                </td>
                             </tr>
                             <?php
                             }
