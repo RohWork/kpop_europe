02318a5b6d94a8fc4634789f7acee4be6fabf8bf
ict_team <ict_team@DESKTOP-M7D75CU>
2023-06-08 10:16:46 +0900

    닉네임 카드 서버 반영

--- application/views/community_detail.php
+++ application/views/community_detail.php
@@ -98,6 +98,7 @@
                                 <td>
                                     <p><?=$cmt->content?></p>
                                     <text id="re_comment_<?=$cmt->idx?>" class="form-control d-none" />
+                                    <button type="button" class="d-none btn btn-primary"><?=$this->lang->line('save')?></button>
                                 </td>
                                 
                                 <td>
