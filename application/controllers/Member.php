fddcc0bddeb5bb3cf33f21596f9f070d24c24804
ict_team <ict_team@DESKTOP-M7D75CU>
2023-07-28 15:34:04 +0900

    랭킹 API수정

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -154,7 +154,7 @@ class Member extends CI_Controller {
         $config = array(
 	'protocol' => "smtp",
 	'smtp_host' => "smtp.kakao.com",
-	'smtp_port' => "587",//"587", // 465 나 587 중 하나를 사용
+	'smtp_port' => "465",//"587", // 465 나 587 중 하나를 사용
 	'smtp_user' => "roh",
 	'smtp_pass' => "s3628742",
 	'charset' => "utf-8",
