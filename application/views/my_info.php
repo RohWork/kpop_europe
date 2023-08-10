3e037f3e2cdb056a90e7a2709a6345606ca0a531
ict_team <ict_team@DESKTOP-M7D75CU>
2023-08-10 10:41:35 +0900

    랭킹 API수정

--- application/views/my_info.php
+++ application/views/my_info.php
@@ -5,81 +5,86 @@
 
 
 <div class="container">
-            <form method="get">
-                <div class="row">
-                    <div class="col-2">
-                        <label class="form-label">
-                            <strong><?=$this->lang->line('country')?></strong>
-                        </label>
-                    </div>
-                    <div class="col-2">
-                        
-                            <select id="check_country" name="country" class="form-select" onchange="get_country_data()">
-                                <option value=""></option>
-                                <?php foreach($country_list as $cnt){ 
-                                    $search_cnt = "";
-                                    if($user_info['country'] == $cnt['idx']){
-                                        $search_cnt = "selected";
-                                    }
-                                ?>
-                                    <option value="<?=$cnt['idx']?>" <?=$search_cnt?>><?=$cnt['name']?></option>
+    <form method="post" id="info_form">
+        <div class="row">
+            <div class="col-2">
+                <label class="form-label">
+                    <strong><?=$this->lang->line('country')?></strong>
+                </label>
+            </div>
+            <div class="col-2">
 
-                                <?php } ?>
-                            </select>
+                    <select id="check_country" name="country" class="form-select" onchange="get_country_data()">
+                        <option value=""></option>
+                        <?php foreach($country_list as $cnt){ 
+                            $search_cnt = "";
+                            if($user_info['country'] == $cnt['idx']){
+                                $search_cnt = "selected";
+                            }
+                        ?>
+                            <option value="<?=$cnt['idx']?>" <?=$search_cnt?>><?=$cnt['name']?></option>
 
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-2">
-                        <label class="form-label">
-                            <strong><?=$this->lang->line('city')?></strong>
-                        </label>
-                    </div>
-                    <div class="col-2">
-                        
-                            <select id="check_city" name="city" class="form-select">
-                                <option value=""></option>
-                                <?php foreach($city_list as $cty){ 
-                                    
-                                    $search_cty = "";
-                                    if($user_info['city'] == $cty['idx']){
-                                        $search_cty = "selected";
-                                    }
-                                    
-                                ?>
-                                    <option value="<?=$cty['idx']?>" <?=$search_cty?>><?=$cty['name']?></option>
+                        <?php } ?>
+                    </select>
 
-                                <?php } ?>
-                            </select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-2">
-                        <label class="form-label">
-                            <strong><?=$this->lang->line('language')?></strong>
-                        </label>
-                    </div>
-                    <div class="col-2">
-                        <select id="check_langueage" name="language" class="form-select">
-                            <?php 
-                                $lang_array = get_langueage_list();
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <label class="form-label">
+                    <strong><?=$this->lang->line('city')?></strong>
+                </label>
+            </div>
+            <div class="col-2">
 
-                                for($i=0;$i<count($lang_array);$i++){
+                    <select id="check_city" name="city" class="form-select">
+                        <option value=""></option>
+                        <?php foreach($city_list as $cty){ 
 
-                                    $search_lang = "";
-                                    if($user_info['language'] == $lang_array[$i]['id']){
-                                        $search_lang = "selected";
-                                    }
-                                 ?>
+                            $search_cty = "";
+                            if($user_info['city'] == $cty['idx']){
+                                $search_cty = "selected";
+                            }
 
-                                    <option value="<?=$lang_array[$i]['id']?>" <?=$search_lang?>><?=$lang_array[$i]['val']?></option>
-                            <?php
-                                }
-                            ?>
+                        ?>
+                            <option value="<?=$cty['idx']?>" <?=$search_cty?>><?=$cty['name']?></option>
 
-                        </select>
+                        <?php } ?>
+                    </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <label class="form-label">
+                    <strong><?=$this->lang->line('language')?></strong>
+                </label>
+            </div>
+            <div class="col-2">
+                <select id="check_langueage" name="language" class="form-select">
+                    <?php 
+                        $lang_array = get_langueage_list();
 
-                    </div>
-                </div>
-            </form>
+                        for($i=0;$i<count($lang_array);$i++){
+
+                            $search_lang = "";
+                            if($user_info['language'] == $lang_array[$i]['id']){
+                                $search_lang = "selected";
+                            }
+                         ?>
+
+                            <option value="<?=$lang_array[$i]['id']?>" <?=$search_lang?>><?=$lang_array[$i]['val']?></option>
+                    <?php
+                        }
+                    ?>
+
+                </select>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2 col-offset-2">
+                <input type="submit" class="btn btn-primary" />
+            </div>
+        </div>
+    </form>
 </div>
