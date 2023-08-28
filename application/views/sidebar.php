5379b7fb3b8341c4f21b195e0de50f9a23006321
ict_team <ict_team@DESKTOP-M7D75CU>
2023-08-28 13:44:16 +0900

    랭킹 API수정

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -48,7 +48,7 @@
             
 
             <li class="mb-1">
-                <a href="/schedule/calendar?country=1" class="btn btn-toggle align-items-center rounded"><?=$this->lang->line('kpop_schedule')?></a>
+                <a href="/schedule/calendar" class="btn btn-toggle align-items-center rounded"><?=$this->lang->line('kpop_schedule')?></a>
             </li>
             
             <li class="border-top my-3"></li>
