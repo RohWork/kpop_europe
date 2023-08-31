2f3db213fa2e69d0bb94f4f6d81c3086ad0aa011
ict_team <ict_team@DESKTOP-M7D75CU>
2023-08-31 09:48:51 +0900

    랭킹 API수정

--- application/views/community_write.php
+++ application/views/community_write.php
@@ -98,8 +98,11 @@
             
             <div class="row mt-1">
                 <div  class="col-md-6 col-xs-6">
-                    
+                    <div class="form-floating">
                     <input name='postTag' placeholder="#write tags to add below" type="hidden">
+                    <label for="title" class="form-label" >
+                        <?=$this->lang->line('title')?>
+                    </label>
                 </div>
             </div>
             
