78d7208d79982a948439b51e32541f356f763a3d
ROH <ROH@JKROH>
2023-10-15 14:34:35 +0900

    시작
    요청자 : 노진국

--- application/models/Bookmark_model.php
+++ application/models/Bookmark_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
  
-class Favorite_model extends CI_Model {
+class Bookmark_model extends CI_Model {
  
     public function __construct()
     {
