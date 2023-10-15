3d3704a53f976282d4e0781b4835e5f9c3c96513
ROH <ROH@JKROH>
2023-10-15 15:24:52 +0900

    시작
    요청자 : 노진국

--- application/views/detail.php
+++ application/views/detail.php
@@ -153,14 +153,14 @@
             <?php if(($this->session->userdata('level') > 2 || $this->session->userdata('org_idx') == $detail_info['organization_idx']) && empty($mode)){ ?>
             
                 
-                <button type="button" class="btn btn-warning" style="margin : 0 5vh;" onclick="set_modify()"><?=$this->lang->line('modify')?></button>
+                <button type="button" class="btn btn-warning" style="margin : 0 5vh 1vh 5vh;" onclick="set_modify()"><?=$this->lang->line('modify')?></button>
 
 
-                <button type="button" class="btn btn-danger" style="margin :0 5vh;" onclick="set_delete()"><?=$this->lang->line('delete')?></button>
+                <button type="button" class="btn btn-danger" style="margin :0 5vh 1vh 5vh;" onclick="set_delete()"><?=$this->lang->line('delete')?></button>
                 
             <?php } ?>
                 
-                <button type="button" class="btn btn-info" style="margin : 0 5vh;" onclick="self.close();"><?=$this->lang->line('close')?></button>
+                <button type="button" class="btn btn-info" style="margin : 0 5vh 1vh 5vh;" onclick="self.close();"><?=$this->lang->line('close')?></button>
                 
             </div>
         </div>
