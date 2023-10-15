159637bfe9377ddd0fcbe0926ae7252892da81c8
ROH <ROH@JKROH>
2023-10-15 15:22:57 +0900

    시작
    요청자 : 노진국

--- application/views/detail.php
+++ application/views/detail.php
@@ -156,11 +156,11 @@
                     <button type="button" class="btn btn-warning" style="margin : 0 5vh;" onclick="set_modify()"><?=$this->lang->line('modify')?></button>
 
 
-                <button type="button" class="btn btn-danger" style="margin :5vh 0 5vh 0;" onclick="set_delete()"><?=$this->lang->line('delete')?></button>
+                <button type="button" class="btn btn-danger" style="margin :0 5vh;" onclick="set_delete()"><?=$this->lang->line('delete')?></button>
                 
             <?php } ?>
                 
-                <button type="button" class="btn btn-info" style="margin : 5vh 0 5vh 0;" onclick="self.close();"><?=$this->lang->line('close')?></button>
+                <button type="button" class="btn btn-info" style="margin : 0 5vh;" onclick="self.close();"><?=$this->lang->line('close')?></button>
                 
             </div>
         </div>
