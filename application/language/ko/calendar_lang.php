c92a8cc12d2b43cafaa4802f3897c8b31a01ab20
ROH <ROH@JKROH>
2023-10-15 14:28:10 +0900

    시작
    요청자 : 노진국

--- application/language/ko/calendar_lang.php
+++ application/language/ko/calendar_lang.php
@@ -16,4 +16,19 @@
     $lang['detail'] = '상세보기';
     $lang['neterror'] = '네트워크 오류!! 관리자에게 문의 주세요!!';
     $lang['viewall'] = '전체보기';
+    
+    $lang['bookmark'] = "즐겨찾기";
+    $lang['detail'] = "상세보기";
+    $lang['insert'] = "추가";
+    $lang['delete'] = "삭제";
+    $lang['modify'] = "수정";
+    $lang['cancel'] = "취소";
+    $lang['list'] = "목록";
+    $lang['search'] = "검색";
+    $lang['login'] = "로그인";
+    $lang['save'] = "저장";
+    $lang['close'] = "닫기";
+    $lang['reset'] = "초기화";
+    $lang['return'] = "돌아가기";
+    $lang['viewall'] = "전체보기";
 ?>
