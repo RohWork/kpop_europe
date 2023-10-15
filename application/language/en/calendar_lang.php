cf1f994814893e4121fd5e1ea41f1f08329e25f9
ROH <ROH@JKROH>
2023-10-15 14:27:56 +0900

    시작
    요청자 : 노진국

--- application/language/en/calendar_lang.php
+++ application/language/en/calendar_lang.php
@@ -16,4 +16,21 @@
     $lang['detail'] = 'DETAIL';
     $lang['neterror'] = 'Network Error!! take support to web manager!!';
     $lang['viewall'] = 'VIEW ALL';
+    
+    $lang['bookmark'] = "BOOKMARK";
+    $lang['detail'] = "DETAIL";
+    $lang['insert'] = "INSERT";
+    $lang['delete'] = "DELETE";
+    $lang['modify'] = "MODIFY";
+    $lang['cancel'] = "CANCEL";
+    $lang['list'] = "LIST";
+    $lang['search'] = "SEARCH";
+    $lang['login'] = "LOGIN";
+    $lang['save'] = "SAVE";
+    $lang['close'] = "CLOSE";
+    $lang['reset'] = "RESET";
+    $lang['return'] = "RETURN";
+    $lang['searchdate'] = "SEARCH DATE";
+    $lang['viewall'] = "VIEW ALL";
+    
 ?>
