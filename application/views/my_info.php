b29137f619b8b8fe01eb351e161a24b9dea37e0e
ROH <ROH@JKROH>
2023-10-15 14:53:28 +0900

    시작
    요청자 : 노진국

--- application/views/my_info.php
+++ application/views/my_info.php
@@ -5,44 +5,44 @@
 
 
 <div class="container">
-    <form method="post" id="info_form" action="/member/my_bookmark_set">
-        <div class="row">
-            <label class="form-label"><h3><?=$this->lang->line('mybookmark')?></h3></label>
+   
+    <div class="row">
+        <label class="form-label"><h3><?=$this->lang->line('mybookmark')?></h3></label>
+    </div>
+
+    <div class="row" style="padding-top: 20px">
+        <div class="col-12">
+            <table class="table table-striped" style="font-size:0.8rem">
+                <tr>
+                    <th>Type</th>
+                    <th><?=$this->lang->line('startdate')?></th>
+                    <th><?=$this->lang->line('country')?></th>
+                    <th><?=$this->lang->line('city')?></th>
+                    <th>Location</th>
+                </tr>
+                <?php 
+                    foreach($list as $li){
+                ?>        
+                <tr onclick="go_detail(<?=$li['idx']?>)" style="cursor: pointer">
+                    <td><?=$li['type']?></td>
+                    <td><?=$li['start_date']?></td>
+                    <td><?=$li['country_name']?></td>
+                    <td><?=$li['city_name']?></td>
+                    <td><?=$li['space']?></td>
+                </tr>   
+                <?php 
+                    }
+                ?>
+            </table>
         </div>
-        
-        <div class="row" style="padding-top: 20px">
-            <div class="col-12">
-                <table class="table table-striped" style="font-size:0.8rem">
-                    <tr>
-                        <th>Type</th>
-                        <th><?=$this->lang->line('startdate')?></th>
-                        <th><?=$this->lang->line('country')?></th>
-                        <th><?=$this->lang->line('city')?></th>
-                        <th>Location</th>
-                    </tr>
-                    <?php 
-                        foreach($list as $li){
-                    ?>        
-                    <tr onclick="go_detail(<?=$li['idx']?>)" style="cursor: pointer">
-                        <td><?=$li['type']?></td>
-                        <td><?=$li['start_date']?></td>
-                        <td><?=$li['country_name']?></td>
-                        <td><?=$li['city_name']?></td>
-                        <td><?=$li['space']?></td>
-                    </tr>   
-                    <?php 
-                        }
-                    ?>
-                </table>
-            </div>
+    </div>
+    <div class="row" style="margin-top: 15px;">
+
+        <div class="col-4" style="text-align: right">
+            <input type="submit" class="btn btn-primary" value="<?=$this->lang->line('confirm')?>"/>
         </div>
-        <div class="row" style="margin-top: 15px;">
-            
-            <div class="col-4" style="text-align: right">
-                <input type="submit" class="btn btn-primary" value="<?=$this->lang->line('confirm')?>"/>
-            </div>
-        </div>
-    </form>
+    </div>
+
 </div>
 </main>
 <script>
