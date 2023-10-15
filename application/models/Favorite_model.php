25ef58d22ff22ff6100994502573e0cc450c4a97
ROH <ROH@JKROH>
2023-10-15 14:23:59 +0900

    시작
    요청자 : 노진국

--- application/models/Favorite_model.php
+++ application/models/Favorite_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
  
-class Country_model extends CI_Model {
+class Favorite_model extends CI_Model {
  
     public function __construct()
     {
