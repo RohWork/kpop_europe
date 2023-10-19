dc6f0a9370cf17e16543f62b01024e3ddbaf3dfc
ict_team <ict_team@DESKTOP-M7D75CU>
2023-10-20 08:43:37 +0900

    air 파일 수정

--- application/models/Bookmark_model.php
+++ application/models/Bookmark_model.php
@@ -12,11 +12,11 @@ class Bookmark_model extends CI_Model {
         
         $sSql = "   SELECT ki.* ,kf.reg_date, kc.name AS country_name, kci.name AS city_name, ko.name AS organization_name, kf.idx AS mark_idx
                     FROM `kpop_bookmark` as kf 
-                    left join kpop_info as ki on kf.kpop_idx = ki.idx and ( ki.start_date like '$month%' or ki.end_date like '$month%' ) 
+                    left join kpop_info as ki on kf.kpop_idx = ki.idx  
                     LEFT JOIN kpop_country AS kc ON kc.idx = ki.country_idx
                     LEFT JOIN kpop_city AS kci	ON kci.idx = ki.city_idx
                     LEFT JOIN kpop_organization AS ko ON ko.idx = ki.organization_idx 
-                    where kf.state = '1' and user_id = '$user_id'  order by kf.idx desc";
+                    where kf.state = '1' and user_id = '$user_id' and ki.start_date like '$month%' or ki.end_date like '$month%'  order by kf.idx desc";
         
         $query = $this->db->query($sSql);
         return $query->result_array();
