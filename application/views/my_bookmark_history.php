58edbfd84e10d8bc1a9a084a6a2090b5378ff58d
ict_team <ict_team@DESKTOP-M7D75CU>
2024-01-05 15:59:52 +0900

    가입문의글 추가

--- application/views/my_bookmark_history.php
+++ application/views/my_bookmark_history.php
@@ -25,13 +25,13 @@
             
         </div>
         <div class="col-4">
-            <button class="btn btn-secondary" type="button" onclick="location.href='/member/my_bookmark?mode=history&year=<?=$year-1?>'"> 
+            <button class="btn btn-secondary" type="button" onclick="location.href='/member/my_bookmark?mode=before&year=<?=$year-1?>'"> 
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
                     <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
                   </svg>          
             </button>
              <?=$year?> 
-            <button class="btn btn-secondary" type="button" onclick="location.href='/member/my_bookmark?mode=history&year=<?=$year+1?>'"> 
+            <button class="btn btn-secondary" type="button" onclick="location.href='/member/my_bookmark?mode=before&year=<?=$year+1?>'"> 
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
                     <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                 </svg>       
