3508c9ba8c8a334b6bb32e329ac11222e8d0c84d
ROH <ROH@JKROH>
2024-01-15 14:20:22 +0900

    시작
    요청자 : 노진국

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -110,28 +110,29 @@
 
             </div>
         </form>
-    </div>
-    <div class="row" style="margin-top: 20px;">
-                <div class="col-4">
+        <div class="row" style="margin-top: 20px;">
+            <div class="col-4">
 
-                </div>
-                <div class="col-4">
-                    <button class="btn btn-secondary" type="button" onclick="location.href='/member/calendar?year=<?=$year-1?>'"> 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
-                            <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
-                          </svg>          
-                    </button>
-                     <?=$year?> 
-                    <button class="btn btn-secondary" type="button" onclick="location.href='/member/calendar?year=<?=$year+1?>'"> 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-                        </svg>       
-                    </button>
-                </div>
-                <div class="col-4">
-
-                </div>
             </div>
+            <div class="col-4">
+                <button class="btn btn-secondary" type="button" onclick="location.href='/member/calendar?year=<?=$year-1?>'"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                        <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                      </svg>          
+                </button>
+                 <?=$year?> 
+                <button class="btn btn-secondary" type="button" onclick="location.href='/member/calendar?year=<?=$year+1?>'"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                    </svg>       
+                </button>
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
+    </div>
+
     <div id="list_modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
         
         <div class="modal-dialog modal-lg" style="max-height:85%;" role="document">
