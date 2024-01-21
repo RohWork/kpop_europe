e7d3e238e565cf9cd7dbee624a0f5db70bb668a1
ROH <ROH@JKROH>
2024-01-21 22:29:11 +0900

    시작
    요청자 : 노진국

--- application/controllers/Schedule.php
+++ application/controllers/Schedule.php
@@ -24,12 +24,9 @@ class Schedule extends CI_Controller {
         
         
         $search = array();
-        $data = array();
         
-        $year = $this->input->get_post("year");
-        
-        if(empty($year)){
-            $year  = $search['date'] =  date("Y");
+        if(!empty($this->input->get_post("date"))){
+            $search['date'] =  date("Y-m-d", strtotime($this->input->get_post("date")));
         }
         
         $search['country'] = $this->session->userdata('country_idx');
@@ -52,11 +49,53 @@ class Schedule extends CI_Controller {
         }
         $data['organization_list'] = $this->org_md->get_organization();
         
-        $data['mode'] = "calendar";
         
-        $data['calendar'] =  $this->sch_md->get_schedule_calendar($search, $year);
-        $data['year'] = $year;
+        //유럽식으로 표기
+        $search['date'] = $this->input->get_post("date");
         $data['search'] = $search;
+        
+        //---- 오늘 날짜
+        $data['thisyear'] = $thisyear = date('Y'); // 4자리 연도
+        $data['thismonth'] = $thismonth = date('n'); // 0을 포함하지 않는 월
+        $data['today'] = $today = date('j'); // 0을 포함하지 않는 일
+
+        //------ $year, $month 값이 없으면 현재 날짜
+        $data['year'] = $year = $this->input->get('year') !== null ? $this->input->get('year') : $thisyear;
+        $data['month'] =$month =  $this->input->get('month') !== null ?$this->input->get('month') : $thismonth;
+        $data['day'] = $day = $this->input->get('month') !== null ? $this->input->get('month') : $today;
+        
+        $data['calendar'] =  $this->sch_md->get_schedule_cnt($search, $year , sprintf("%02d",$month));
+
+        $data['prev_month'] = $prev_month = $month - 1;
+        $data['next_month'] = $next_month =  $month + 1;
+        $data['prev_year'] = $data['next_year'] = $year;
+        
+        if ($month == 1) {
+            $data['prev_month'] = 12;
+            $data['prev_year'] = $year - 1;
+        } else if ($month == 12) {
+            $data['next_month'] = 1;
+            $data['next_year'] = $year + 1;
+        }
+        $data['preyear'] = $year - 1;
+        $data['nextyear'] = $year + 1;
+
+        $data['predate'] = date("Y-m-d", mktime(0, 0, 0, $month - 1, 1, $year));
+        $data['nextdate'] = date("Y-m-d", mktime(0, 0, 0, $month + 1, 1, $year));
+
+        // 1. 총일수 구하기
+        $data['max_day'] = $max_day =  date('t', mktime(0, 0, 0, $month, 1, $year)); // 해당월의 마지막 날짜
+        //echo '총요일수'.$max_day.'<br />';
+
+        // 2. 시작요일 구하기
+        $data['start_week'] = $start_week =  date("w", mktime(0, 0, 0, $month, 1, $year)); // 일요일 0, 토요일 6
+
+        // 3. 총 몇 주인지 구하기
+        $data['total_week'] = ceil(($max_day + $start_week) / 7);
+
+        // 4. 마지막 요일 구하기
+        $data['last_week'] = date('w', mktime(0, 0, 0, $month, $max_day, $year));
+
         
         if(confirm_mobile()){ //MOBILE
             $this->load->view('/mobile/header');
