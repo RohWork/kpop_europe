636bb2eb17eb88a08944e85c30c2af57473f8451
ROH <ROH@JKROH>
2024-03-22 15:50:28 +0900

    시작
    요청자 : 노진국

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -70,7 +70,7 @@
             <li class="border-top my-3"></li>
             
             <li class="mb-1">
-                <a href="/schedule/calendar" class="btn btn-toggle align-items-center rounded"><?=$this->lang->line('kpop_schedule')?></a>
+                <a href="/schedule/list" class="btn btn-toggle align-items-center rounded"><?=$this->lang->line('kpop_schedule')?></a>
             </li>
             <!--
             <li class="border-top my-3"></li>
