8b587e3c43e77bce12332e6147278b5940de2e17
ROH <ROH@JKROH>
2024-05-05 09:31:55 +0900

    시작
    요청자 : 노진국

--- application/views/main.php
+++ application/views/main.php
@@ -7,7 +7,29 @@
 <div class="container">
     <div class="row">
         <div class="col-4">
-            <h4>1</h4>
+            <h4><?=$this->lang->line('fast_schedule')?></h4>
+            <table class="table">
+                <tr>
+                    <th><?=$this->lang->line('date')?></th>
+                    <th><?=$this->lang->line('location')?></th>
+                </tr>
+                <?php 
+                    if(!empty($schedule_list)){
+                        foreach($schedule_list as $schedule){
+                ?>
+                    <tr>
+                        <td><?=$schedule['start_date']?></td><td><?=$schedule['location']?></td>
+                    </tr>
+                <?php
+                            
+                        }
+                    }
+                ?>
+                <tr>
+                    
+                </tr>
+                
+            </table>
         </div>
         <div class="col-4">
             <h4>2</h4>
