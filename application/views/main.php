91f85d75ea30b0ea08384ba79244160a27c7222d
ROH <ROH@JKROH>
2024-05-05 09:10:51 +0900

    시작
    요청자 : 노진국

--- application/views/main.php
+++ application/views/main.php
@@ -1,4 +1,20 @@
-<div>
-    
+<?php
+    $lang = $this->session->userdata('lang');
+    $this->lang->load('view', $lang);
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <h4></h4>
+        </div>
+        <div class="col-4">
+            
+        </div>
+        <div class="col-4">
+            
+        </div>
+    </div>
 </div>
 </main>
