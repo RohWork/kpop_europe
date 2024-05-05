5b5d8c8bdc37193440e0477126505b493fe17efb
ROH <ROH@JKROH>
2024-05-05 09:11:01 +0900

    시작
    요청자 : 노진국

--- application/views/main.php
+++ application/views/main.php
@@ -7,13 +7,13 @@
 <div class="container">
     <div class="row">
         <div class="col-4">
-            <h4></h4>
+            <h4>1</h4>
         </div>
         <div class="col-4">
-            
+            <h4>2</h4>
         </div>
         <div class="col-4">
-            
+            <h4>3</h4>
         </div>
     </div>
 </div>
