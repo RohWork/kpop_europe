290c2df5ea30a9cfbcb44f1282d1205e3a5e8f07
ROH <ROH@JKROH>
2024-07-21 10:52:49 +0900

    시작
    요청자 : 노진국

--- application/views/main_map.php
+++ application/views/main_map.php
@@ -37,7 +37,7 @@
         function initMap() {
 
             
-            const map = new google.maps.Map(document.getElementById("map"), {
+             map = new google.maps.Map(document.getElementById("map"), {
               zoom: 4,
               center: { lat: 33, lng: 151 },
               disableDefaultUI: true,
