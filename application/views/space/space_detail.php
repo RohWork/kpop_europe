16c80baf081e611874a67d69b3e28c9320e1babd
ROH <ROH@JKROH>
2024-09-21 12:40:15 +0900

    시작
    요청자 : 노진국

--- application/views/space/space_detail.php
+++ application/views/space/space_detail.php
@@ -58,7 +58,9 @@
                         <label class="form-label bold"><strong><?=$this->lang->line('address')?></strong></label>
                     </div>
                     <div class="col-8">
-                        <input type="text" id="space_location" name="space_location" value="<?=$detail_info['space_location']?>" class="form-control"/>
+                        <textarea id="space_location" name="space_location"  class="form-control">
+                                <?=$detail_info['space_location']?>
+                        </textarea>
                     </div>
                 </div>
                 <div class="row" style="padding-top: 5px">
