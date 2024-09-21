a879cffa2e28adc77b4de78abb2652e558809c43
ROH <ROH@JKROH>
2024-09-21 11:42:50 +0900

    시작
    요청자 : 노진국

--- application/views/space/space_detail.php
+++ application/views/space/space_detail.php
@@ -50,6 +50,8 @@
                             <?php } ?>
                         </select>
                     </div>
+                    
+                    
                 </div>
                 <div class="row" style="padding-top: 5px">
                     <div class="col-4">
@@ -60,7 +62,9 @@
                     </div>
                 </div>
                 <div class="row" style="padding-top: 5px">
-                    
+                    <div id="map" style="position: relative; overflow: hidden;">
+                        
+                    </div>
                 </div>
             </form>
         </div>
@@ -117,4 +121,26 @@
             }
         }
     </script>
+    
+    
+    <script>
+    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+      key: "AIzaSyBu5yucZu09lR2UwMGYPFGu3V9FIQL2hYo",
+      v: "weekly",
+      // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+      // Add other bootstrap parameters as needed, using camel case.
+    });
+    let map;
+
+    async function initMap() {
+      const { Map } = await google.maps.importLibrary("maps");
+
+      map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    }
+
+    initMap();
+</script>
 </html>
