55713b7d24c010fcb26dfabbb3adfbf28b198a65
ROH <ROH@JKROH>
2024-09-21 10:59:42 +0900

    시작
    요청자 : 노진국

--- application/views/space/space_list.php
+++ application/views/space/space_list.php
@@ -94,7 +94,7 @@
 
     <div class="modal fade" id="detail_modal" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
         
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title"><?=$this->lang->line('detail')?></h5>
