4e5d0102578cd20c5f81f833fdc20c56d0e17d5f
ROH <ROH@JKROH>
2024-09-21 10:43:55 +0900

    시작
    요청자 : 노진국

--- application/models/Space_model.php
+++ application/models/Space_model.php
@@ -14,16 +14,19 @@ class Space_model extends CI_Model {
         
         if(!empty($search['country'])){
             if($search['country'] != 'all'){
-                $where .= " AND country_idx ='".$search['country']."'";
+                $where .= " AND kp.country_idx ='".$search['country']."'";
             }
         }
         if(!empty($search['city'])){
             if($search['city'] != 'all'){
-                $where .= " AND .city_idx ='".$search['city']."'";
+                $where .= " AND kp.city_idx ='".$search['city']."'";
             }
         }
         
-        $sSql = "SELECT * FROM kpop_space WHERE 1=1 $where";
+        $sSql = "SELECT * FROM kpop_space as kp WHERE 1=1"
+                . "left join kpop_country as ko on kc.country_idx = ko.idx "
+                ."left join kpop_city as kc on kc.country_idx = ko.idx "
+                . "where 1=1 $where";
         
 
         $query = $this->db->query($sSql);
