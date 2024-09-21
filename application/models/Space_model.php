7d64aa3ec4a372f961e3a74f78daa82fbfe2df2c
ROH <ROH@JKROH>
2024-09-21 10:28:50 +0900

    시작
    요청자 : 노진국

--- application/models/Space_model.php
+++ application/models/Space_model.php
@@ -24,6 +24,7 @@ class Space_model extends CI_Model {
         }
         
         $sSql = "SELECT * FROM kpop_space WHERE 1=1 $where";
+        echo $sSql;
         
         $query = $this->db->query($sSql);
         return $query->result_array();
