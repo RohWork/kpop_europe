23054e374925a066d0ca98b90138aa5c18b8c071
ROH <ROH@JKROH>
2024-09-21 12:46:20 +0900

    시작
    요청자 : 노진국

--- application/controllers/Space.php
+++ application/controllers/Space.php
@@ -57,39 +57,58 @@ class Space extends CI_Controller {
         
         $data['detail_info'] = $detail = $this->space_md->get_space_idx($idx);
 
-        
         $data['country_list'] = $this->cont_md->get_country();
         
-
         $data['city_list'] = $this->city_md->get_city($detail['city_idx']);
-
+        $data['space_idx'] = $idx;
   
         $this->load->view('space/space_detail',$data);
+    }
+    
+    public function modify_ajax(){
+        $data = array();
+        
+        $data['result'] = 200;
+        $data['message'] = "";
+        
+        $params = array();
+        
+        $params['country_idx'] = $this->input->post("country_idx");
+        $params['city_idx'] = $this->input->post("city_idx");
+        $params['space_name'] = $this->input->post("space_name");
+        $params['space_location'] = $this->input->post("space_location");
+        $params['space_x'] = $this->input->post("space_x");
+        $params['space_y'] = $this->input->post("space_y");
+        $idx = $this->input->post("space_idx");
+        
+        $result = $this->space_md->modify_space($params, $idx);
+        
+        if(!$result){
+            $data['result'] = 400;
+            $data['message'] = $this->lang->line('dataerror');
+        }
+        
+        header("Content-Type: application/json;");
+        echo json_encode($data);
+    }
+    
+
+    
+    public function delete_ajax(){
+        
     }
     
     public function insert(){
         
     }
     
-    public function add_set(){
+    public function insert_ajax(){
         
     }
     
-    public function modify(){
-        
-    }
+
     
-    public function modify_set(){
-        
-    }
-    
-    public function delete(){
-        
-    }
-    
-    public function delete_set(){
-        
-    }
+
     
     
     
