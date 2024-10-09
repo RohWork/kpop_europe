879d98fa05958fe184cec77772dc319307f8d576
ROH <ROH@JKROH>
2024-10-09 09:50:48 +0900

    시작
    요청자 : 노진국

--- application/controllers/Space.php
+++ application/controllers/Space.php
@@ -115,7 +115,10 @@ class Space extends CI_Controller {
         $search['country'] = $this->input->get_post("country_idx");
 
         $search['city'] = $this->input->get_post("city_idx");
-
+        
+        if(empty($search['country'])){
+            $search['country'] = 1;
+        }
         
         $data['country_list'] = $this->cont_md->get_country();
         
