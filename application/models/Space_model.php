cc1aa7da1c10a3244dc649c5d11ad28a10838ef5
ROH <ROH@JKROH>
2024-10-09 10:15:50 +0900

    시작
    요청자 : 노진국

--- application/models/Space_model.php
+++ application/models/Space_model.php
@@ -60,4 +60,12 @@ class Space_model extends CI_Model {
         return $this->db->affected_rows();
     }
     
+    function insert_space($params){
+        
+        $params['regi_date'] = date('Y-m-d h:i:s');
+        $this->db->insert('kpop_space', $params);
+        
+        return $this->db->insert_id();
+    }
+    
 }
