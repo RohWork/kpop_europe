5d19f9a148a94c6715de952c7c88d19175f62ed7
ROH <ROH@JKROH>
2024-10-09 09:54:20 +0900

    시작
    요청자 : 노진국

--- application/views/space/space_insert.php
+++ application/views/space/space_insert.php
@@ -4,7 +4,7 @@
     
     $space_x = "";
     $space_y = "";
-
+    $zoom = "";
 ?>
 
 <html>
@@ -31,7 +31,7 @@
                                         
                                         $space_x = $cont['space_x'];
                                         $space_y = $cont['space_y'];
-                                        
+                                        $zoom = "14";
                                     }
                             ?>
                                 
@@ -53,7 +53,7 @@
                                         
                                         $space_x = $city['space_x'];
                                         $space_y = $city['space_y'];
-                                        
+                                        $zoom = "18";
                                     }
                             ?>
                                 
