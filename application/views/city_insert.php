b225a7379a4e88a289b41c62cdf8b4cc6a1707ed
JYJK <JYJK@DESKTOP-69R7O8H>
2025-12-06 10:43:19 +0900

    map 박스로 리뉴얼 by 노진국

--- application/views/city_insert.php
+++ application/views/city_insert.php
@@ -73,7 +73,7 @@
                     </div>
                     <div class="col-8">
                         <div class="d-flex">
-                            <div class="geocoder-wrap" id="geocoder"></div>
+                          
                             <input type="hidden" id="address" name="address" placeholder="검색 또는 핀 이동 시 자동 입력" readonly>
                         </div>
                     </div>
