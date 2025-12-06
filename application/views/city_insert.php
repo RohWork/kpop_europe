707339b22d713201172693ba77bed02a8dbc714f
JYJK <JYJK@DESKTOP-69R7O8H>
2025-12-06 10:35:28 +0900

    map 박스로 리뉴얼 by 노진국

--- application/views/city_insert.php
+++ application/views/city_insert.php
@@ -1,7 +1,19 @@
 <?php
     $lang = $this->session->userdata('lang');
     $this->lang->load('view', $lang);
-
+    if(!empty($city_name)){
+?>
+        <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js"></script>
+        <script src="https://kit.fontawesome.com/89b002c3b7.js" crossorigin="anonymous"></script>
+        <script src="/asset/js/jquery.cookie-1.4.1.min.js" crossorigin="anonymous"></script>
+<?php
+    }
 ?>
 
     <div class="container">
